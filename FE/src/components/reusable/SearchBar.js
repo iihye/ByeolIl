4bf0f8edaafea7c160fa4d29d3394c06aed59e95
euny0ung <hdtv0218@gmail.com>
2024-01-17 13:00:25 +0900

    Fix: 영어 검색 오류 해결

--- FE/src/components/reusable/SearchBar.js
+++ FE/src/components/reusable/SearchBar.js
@@ -1,119 +1,43 @@
 import React, { useState, useEffect } from 'react';
-
-const dummyItems = {
-    data: [
-        {
-            boardIndex: 1,
-            userIndex: 1,
-            boardRegTime: '2014-01-02',
-            boardInputTime: '2014-01-02',
-            boardContent: '조회한 글입니다',
-            boardLocation: 1,
-            boardAccess: 'OPEN',
-            boardLike: 3,
-            tagContent: ['밥', '저녁', '싸피'],
-        },
-        {
-            boardIndex: 2,
-            userIndex: 2,
-            boardRegTime: '2014-01-04',
-            boardInputTime: '2014-01-04',
-            boardContent: '조회한 글입니다2',
-            boardLocation: 2,
-            boardAccess: 'OPEN',
-            boardLike: 4,
-            tagContent: ['오운완', '운동', '일기'],
-        },
-        {
-            boardIndex: 2,
-            userIndex: 2,
-            boardRegTime: '2014-01-04',
-            boardInputTime: '2014-01-04',
-            boardContent: '조회한 글입니다3',
-            boardLocation: 2,
-            boardAccess: 'OPEN',
-            boardLike: 4,
-            tagContent: ['오운완', '운동', '일기'],
-        },
-        {
-            boardIndex: 3,
-            userIndex: 2,
-            boardRegTime: '2014-01-04',
-            boardInputTime: '2014-01-04',
-            boardContent: '내 이름은 김조회',
-            boardLocation: 2,
-            boardAccess: 'OPEN',
-            boardLike: 4,
-            tagContent: ['오운완', '운동', '일기'],
-        },
-        {
-            boardIndex: 4,
-            userIndex: 2,
-            boardRegTime: '2014-01-04',
-            boardInputTime: '2014-01-04',
-            boardContent: '이것 도검색 해보시 지 마지막테스트',
-            boardLocation: 2,
-            boardAccess: 'OPEN',
-            boardLike: 4,
-            tagContent: ['오운완', '운동', '일기'],
-        },
-        {
-            boardIndex: 5,
-            userIndex: 2,
-            boardRegTime: '2014-01-04',
-            boardInputTime: '2014-01-04',
-            boardContent: '문자가 길어지면 인식을 못하는걸까요?',
-            boardLocation: 2,
-            boardAccess: 'OPEN',
-            boardLike: 4,
-            tagContent: ['오운완', '운동', '일기'],
-        },
-        {
-            boardIndex: 5,
-            userIndex: 2,
-            boardRegTime: '2014-01-04',
-            boardInputTime: '2014-01-04',
-            boardContent: 'hello hi',
-            boardLocation: 2,
-            boardAccess: 'OPEN',
-            boardLike: 4,
-            tagContent: ['오운완', '운동', '일기'],
-        },
-        {
-            boardIndex: 5,
-            userIndex: 2,
-            boardRegTime: '2014-01-04',
-            boardInputTime: '2014-01-04',
-            boardContent: 'ohohoh',
-            boardLocation: 2,
-            boardAccess: 'OPEN',
-            boardLike: 4,
-            tagContent: ['오운완', '운동', '일기'],
-        },
-    ],
-};
+import axios from "axios";
 
 function SearchBar() {
     const [searchValue, setSearchValue] = useState('');
     const [filterData, setFilterData] = useState([]);
+    const [dummyItems, setDummyItems]=useState([]);
 
     const handleSearchValue = (e) => {
-        setSearchValue(e.target.value);
+        setSearchValue(e.target.value); 
     };
 
+    // 추후 url https://7e030bec-d09a-467e-93a6-3b1848ed02c4.mock.pstmn.io/board/list/{userIndex}로 변경, 의존성 배열에 userIndex 넣기
+    useEffect(()=>{
+
+        const fetchData=async()=>{
+            await axios.get("https://7e030bec-d09a-467e-93a6-3b1848ed02c4.mock.pstmn.io/board/list/1")
+            .then((response)=>{
+            setDummyItems(response.data);
+            setFilterData(response.data)
+        }).catch((e)=>console.log(e));
+        }
+        fetchData();
+
+    },[])
+
+    // console.log(dummyItems[0])
+
     useEffect(() => {
-        const filterContents = dummyItems.data.filter((it) =>
+        const filterContents = dummyItems.filter((it) =>
             it.boardContent
                 .toLocaleLowerCase()
                 .replace(/\s/g, '')
                 .includes(searchValue.toLocaleLowerCase().replace(/\s/g, ''))
         );
 
-        console.log(filterData);
-
-        // 필터링이 완료된 후에 상태를 업데이트합니다.
+        // 필터링이 완료된 후에 상태를 업데이트
         setFilterData(filterContents);
     }, [searchValue]);
+
 
     return (
         <div className="searchBar">
