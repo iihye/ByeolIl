939e28eb8bc592a5bfad6681eee840dd2fc54ccf
조한빈 <whgksqls9999@naver.com>
2024-01-17 13:49:13 +0900

    Feat: Modal 공통 컴포넌트 작성

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,5 +1,32 @@
+//Find
+import FindID from "./components/login/FindID";
+import FindPW from "./components/login/FindPW";
+// Modal
+import RadioModal from "./components/radio/RadioModal";
+import StarDetail from "./components/star/StarDetail";
+// Alert
+import StarDeleteAlert from "./components/star/StarDeleteAlert";
+import StarReportAlert from "./components/star/StarReportAlert";
+import BlockAlert from "./components/user/BlockAlert";
+import PWCheck from "./components/user/PWCheckAlert";
+
 function App() {
-  return <div className="App"></div>;
+  console.log("APP RENDERED");
+  const REST_API_KEY = "51d0ffff4a682ea6be213544986554b9";
+  const REDIRECT_URI = "http://localhost:3000/";
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const loginHandler = () => {
+    window.location.href = link;
+  };
+
+  return (
+    <div className="App">
+      <RadioModal />
+      <br />
+      <StarDetail starIndex={1} />
+    </div>
+  );
 }
 
 export default App;
