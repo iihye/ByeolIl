79a035b4295cade2a5c6bd061f779bac3126ba0a
조한빈 <whgksqls9999@naver.com>
2024-01-18 17:10:04 +0900

    Chore: Alert 처리 로직 구성

--- FE/src/components/reusable/Alert.js
+++ FE/src/components/reusable/Alert.js
@@ -1,33 +1,106 @@
+import { useEffect, useRef } from "react";
+import axios from "axios";
+
 // type: 'report', 'PWCheck', 'delete', 'block'
-function Alert({ type }) {
+function Alert({ type, boardIndex }) {
   return (
     <div className="alert">
       {type === "block" ? <Block /> : null}
-      {type === "delete" ? <Delete /> : null}
+      {type === "delete" ? <Delete boardIndex={boardIndex} /> : null}
       {type === "PWCheck" || type === "report" ? <InputAlert type={type} /> : null}
     </div>
   );
 }
 
 function InputAlert({ type }) {
+  const input = useRef(null);
+
   const toEnter = type === "PWCheck" ? "비밀번호를" : "신고 내용을";
-  const button = type === "PWCheck" ? "입력" : "신고";
+  const buttonValue = type === "PWCheck" ? "입력" : "신고";
+  const URL = ""; // axios 요청 URL
+
+  // 빈 내용 체크
+  const emptyCheck = (input) => {
+    return input.trim();
+  };
+
+  const reqReport = (inputData) => {
+    const data = {
+      boardIndex: "", // 게시글 번호
+      userindex: "", // 유저 번호
+      reportContent: inputData,
+    };
+
+    axios
+      .post(`${URL}/board/report`, {}, { params: data })
+      .then((res) => {
+        /* 1. 신고 완료 모달 띄우기 */
+        /* 2. 신고 완료 모달 닫기 */
+        /* 3. 신고 내용 입력 모달 닫기 */
+      })
+      .catch((e) => console.log(e));
+  };
+
+  // 비밀번호 입력 / 신고 요청
+  const handleSubmit = () => {
+    const inputData = input.current.value;
+
+    if (!emptyCheck(inputData)) {
+      alert(`${toEnter} 입력해주세요!`);
+      return;
+    }
+
+    if (buttonValue === "신고") {
+      reqReport(inputData);
+    } else if (buttonValue === "입력") {
+      /* 1. 비밀번호 일치하는지 체크 */
+      /* 2. - 일치할 경우) 해당 모달 내리고 개인정보 수정 모달 띄우기 */
+      /* 3. - 일치하지 않을 경우) '비밀번호가 일치하지 않습니다.' 띄우기 */
+    }
+  };
+
+  useEffect(() => {
+    // Enter 키 입력으로 input 내용 처리하기
+    const handleEnter = (e) => {
+      if (e.key === "Enter") {
+        handleSubmit();
+      }
+    };
+    input.current.addEventListener("keydown", handleEnter);
+
+    return () => {
+      clearTimeout(handleEnter);
+    };
+  }, []);
 
   return (
     <>
       <div>{toEnter} 입력해주세요.</div>
       <div>
-        <input />
+        <input ref={input} />
       </div>
       <div>
-        <button>{button}</button>
+        <button onClick={handleSubmit}>{buttonValue}</button>
         <button>취소</button>
       </div>
     </>
   );
 }
 
-function Delete() {
+function Delete({ boardIndex }) {
+  const reqStarDelete = () => {
+    const data = {
+      boardIndex: boardIndex,
+      userIndex: "",
+    };
+
+    const URL = "";
+
+    /* 게시글 삭제  요청 보내기 */
+    /* - 게시글 정상 삭제시) 해당 alert 닫기 -> 게시글 modal 닫기*/
+    /* - 게시글 삭제 실패시) 오류 모달 띄우기? */
+  };
+
   return (
     <>
       <div>
@@ -36,7 +109,7 @@ function Delete() {
         정말로.. 삭제할까요?
       </div>
       <div>
-        <button>삭제</button>
+        <button onClick={() => {}}>삭제</button>
         <button>취소</button>
       </div>
     </>
@@ -44,6 +117,8 @@ function Delete() {
 }
 
 function Block() {
+  /* 1. axios로 해당 사용자 차단 해제일 받아오기 */
+  /* 2. 받아온 해제일 아래 alert에 채우기*/
   return (
     <>
       <div>
