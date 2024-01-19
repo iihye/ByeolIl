b04fc0fad47b28069612d5de28683fc27e213e3d
조한빈 <whgksqls9999@naver.com>
2024-01-19 16:11:50 +0900

    Feat: Reply 컴포넌트 작성

--- FE/src/App.js
+++ FE/src/App.js
@@ -9,17 +9,16 @@ import StarDeleteAlert from "./components/star/StarDeleteAlert";
 import StarReportAlert from "./components/star/StarReportAlert";
 import BlockAlert from "./components/user/BlockAlert";
 import PWCheck from "./components/user/PWCheckAlert";
+// Reply
+import StarReplyList from "./components/star/StarReplyList";
+import StarReplyListItem from "./components/star/StarReplyListItem";
 
 function App() {
   return (
-    <div className="App">
-      <StarDeleteAlert />
-      <br />
-      <BlockAlert />
-      <br />
-      <StarReportAlert />
-      <br />
-      <PWCheck />
+    <div>
+      <div>
+        <StarReplyList />
+      </div>
     </div>
   );
 }
