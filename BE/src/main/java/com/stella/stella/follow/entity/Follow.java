1209acc14b7d17443447e03f6bebf3030c4c2632
Lee Hye Jin <floral_berry@naver.com>
2024-01-21 18:37:00 +0900

    Feat: Follow entity 추가

--- BE/src/main/java/com/stella/stella/follow/entity/Follow.java
+++ BE/src/main/java/com/stella/stella/follow/entity/Follow.java
@@ -1,4 +1,27 @@
 package com.stella.stella.follow.entity;
 
+import com.stella.stella.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "follow")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Follow {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long followIndex;
+
+    @Id
+    @Column(name = "to_member_index")
+    private Long toMemberIndex;
+
+    @Id
+    @Column(name = "from_member_index")
+    private Long fromMemberIndex;
 }
