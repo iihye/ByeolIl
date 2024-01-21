2773ab0b0b2a7a62fda31d8ca020fac8419e8880
euny0ung <hdtv0218@gmail.com>
2024-01-21 18:22:25 +0900

    Feat: 라디오 범위 설정 기능 추가

--- FE/src/components/user/Settings.js
+++ FE/src/components/user/Settings.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-// 어떤 식으로 작동하는건지 모르겠다??
-//
 function Settings() {
+    const options = [
+        { name: '흐린', value: 'OLD' },
+        { name: '아련한', value: 'OLD' },
+        { name: '어렴풋한', value: 'OLD' },
+    ];
+
+    const [settingValue, setSettingValue] = useState('');
+
+    // userIndex, options value는 실제 API 완성되면 수정
+    const handleOption = (e) => {
+        axios
+            .put('https://7e030bec-d09a-467e-93a6-3b1848ed02c4.mock.pstmn.io/user/setting', {
+                userIndex: '1',
+                userRadio: e.target.value,
+            })
+            .then((response) => {
+                const result =
+                    response.data.response === 'success' && response.status === 200 ? '변경 성공' : '변경 실패';
+                setSettingValue(result);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+
     return (
         <div className="Settings">
             <div>환경설정</div>
             <div>
-                <button>흐린</button>
-                <button>아련한</button>
-                <button>어렴풋한</button>
+                {options.map((option) => {
+                    return (
+                        <button onClick={handleOption} value={option.value}>
+                            {option.name}
+                        </button>
+                    );
+                })}
+                <div>{settingValue}</div>
             </div>
         </div>
     );
