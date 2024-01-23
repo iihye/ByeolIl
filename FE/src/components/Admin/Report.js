d4dcc06e5fdec80d1978a00a7de7f23ae7b04f89
euny0ung <hdtv0218@gmail.com>
2024-01-23 16:45:58 +0900

    Fix: 실제 API 연동 전까지 보류
    Change-Id: Ie652d31b6bc2fabc5d538124c71c4af56dfb6a8d

--- FE/src/components/Admin/Report.js
+++ FE/src/components/Admin/Report.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 function Report() {
     const [reportData, setReportData] = useState([]);
-    const [boardData, setBoardData] = useState([]);
+    const [boardData, setBoardData] = useState([]); // 게시글 전체 항목
+    const [boardContent, setBoardContent] = useState([]); // 게시글에서 boardContent만 뽑아옴
 
     useEffect(() => {
         const fetchData = async () => {
@@ -16,35 +17,68 @@ function Report() {
         };
 
         fetchData();
+    }, []);
 
-        //  boardIndex를 매개변수로 넘겨 일치하는 데이터 받아오기
-
+    //  boardIndex를 매개변수로 넘겨 일치하는 데이터 받아오기
+    useEffect(() => {
         const fetchBoardData = async () => {
-            Promise.all(
-                reportData.map(async (it) => {
-                    return await axios
-                        .get(
-                            `https://d9434a94-4844-4787-a437-ceb2559ee35c.mock.pstmn.io/board/${it.boardIndex}`
-                        )
-                        .then((response) => {
-                            setBoardData(response.data);
-                        });
-                })
-            );
+            try {
+                await Promise.all(
+                    reportData &&
+                        reportData.map(async (it) => {
+                            const response = await axios.get(
+                                `https://d9434a94-4844-4787-a437-ceb2559ee35c.mock.pstmn.io/board/${it.boardIndex}`
+                            );
+                            setBoardData((prevBoardData) => [
+                                ...prevBoardData,
+                                response.data,
+                            ]);
+                        })
+                );
+            } catch (error) {
+                console.error(error);
+            }
+
+            console.log(boardData);
         };
 
         fetchBoardData();
-    }, []);
+    }, [reportData]);
+
+    useEffect(() => {
+        const fetchContentData = async () => {
+            // 예외처리
+            if (boardData.length === 0) {
+                console.log('Board data is not loaded yet.');
+                return;
+            }
+            const contentResponse = await Promise.all(
+                boardData &&
+                    boardData.map(async (res, index) => {
+                        return res.boardContent;
+                    })
+            );
+            setBoardContent(contentResponse);
+        };
+        fetchContentData();
+    }, [boardData]);
+
+    // console.log(boardContent);
 
     return (
         <div className="Report">
-            {reportData.map((it) => (
-                <li key={it.reportIndex}>
-                    {it.reportInputDate}
-                    {it.userNickname}
-                    {it.reportContent}
-                </li>
-            ))}
+            {reportData.length > 0 && boardContent.length > 0 ? (
+                reportData.map((it, index) => (
+                    <li key={it.reportIndex}>
+                        {boardContent[index]}
+                        {it.reportInputDate}
+                        {it.userNickname}
+                        {it.reportContent}
+                    </li>
+                ))
+            ) : (
+                <div>Loading...</div>
+            )}
         </div>
     );
 }
