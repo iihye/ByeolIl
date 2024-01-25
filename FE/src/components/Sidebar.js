71fa87d4c21111329cf35d326e5bd820b93afa44
euny0ung <hdtv0218@gmail.com>
2024-01-25 11:02:36 +0900

    Feat: 관리자 모드 구현
    Change-Id: Ic76beff63b5cdf446a0db9384fe5cf6c20ef7f19

--- FE/src/components/Sidebar.js
+++ FE/src/components/Sidebar.js
@@ -1,59 +1,68 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
 // import StarFavoList from './star/StarFavorList'
 // import FollowList from './user/FollowList'
 // import FindUser from './user/FindUser'
 // import StarTagSearch from './star/StarTagSearch'
 // 환경설정 컴포넌트..?
 // import Alarm from './user/Alarm'
-import {Link} from "react-router-dom"
+import { Link } from 'react-router-dom';
+
 const items = [
-    { name: "회원정보수정", path:"/ChangeInfo"},
-    { name: "별리스트", path:"/StarList"},
-    { name: "좋아하는별리스트", path:"/StarFavoList"},
+    { name: '회원정보수정', path: '/ChangeInfo' },
+    { name: '별리스트', path: '/StarList' },
+    { name: '좋아하는별리스트', path: '/StarFavoList' },
 ];
 
-function SideBarItem({item}) {
+// 관리자라면 사이드바 메뉴에 신고 관리가 추가됨
+const isAdmin = localStorage.getItem('memberId') === 'admin';
+
+const menuItems = isAdmin
+    ? [...items, { name: '신고 관리', path: '/Report' }]
+    : items;
+
+// 로그아웃
+const handleLogout = () => {
+    localStorage.removeItem('token');
+    window.location.reload();
+};
+
+function SideBarItem({ item }) {
     return (
         <div className="sidebar-item">
-          <p>{item.name}</p>
+            <p>{item.name}</p>
         </div>
-      );
+    );
 }
 
-function SidebarList (props) {
+function SidebarList(props) {
     return (
         <div className="SidebarList">
             <ul>
                 <h2>{props.name}님의 우주</h2>
-                {items.map((item, index) => {
+                {menuItems.map((item, index) => {
                     return (
                         <Link to={item.path} key={index}>
-                            <SideBarItem
-                                item={item}
-                            />
+                            <SideBarItem item={item} />
                         </Link>
                     );
                 })}
-                <button>로그아웃</button>
+                <button onClick={handleLogout}>로그아웃</button>
             </ul>
         </div>
-    )
+    );
 }
 
-export default function Sidebar (props) {
-    const [viewSideBar ,setViewSideBar] = useState(false);
+export default function Sidebar(props) {
+    const [viewSideBar, setViewSideBar] = useState(false);
 
-    function toggleSidebar () {
-        if(viewSideBar === false) setViewSideBar(true); 
-        else setViewSideBar(false);
+    function toggleSidebar() {
+        setViewSideBar(!viewSideBar);
     }
 
     return (
         <div className="Sidebar">
             <button onClick={toggleSidebar}>=</button>
-            <div>
-                {viewSideBar ? <SidebarList name={props.name}/> : <div/>}
-            </div>
+            <div>{viewSideBar && <SidebarList name={props.name} />}</div>
         </div>
-    )
+    );
 }
--- FE/src/pages/MainPage.js
+++ FE/src/pages/MainPage.js
@@ -1,20 +1,18 @@
-import React from "react";
-import {atom, useRecoilState} from "recoil";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { atom, useRecoilState } from 'recoil';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // import Login from '../components/login/Login'
 // import Regist from '../components/login/Regist'
-import Sidebar from "../components/Sidebar";
-
-import ChangeInfo from '../components/user/ChangeInfo'
-import StarList from '../components/star/StarList'
+import Sidebar from '../components/Sidebar';
+import ChangeInfo from '../components/user/ChangeInfo';
+import StarList from '../components/star/StarList';
 // import StarFavoList from './star/StarFavorList'
 // import FollowList from './user/FollowList'
 // import FindUser from './user/FindUser'
 // import StarTagSearch from './star/StarTagSearch'
 // 환경설정 컴포넌트..?
 // import Alarm from './user/Alarm'
-
 
 // - 로그인 전
 //  - 우측 상단의 로그인, 회원가입 버튼을 누를 수 있다
@@ -28,29 +26,32 @@ const loginState = atom({
 
 export default function MainPage() {
     const [isLogin, setIsLogin] = useRecoilState(loginState);
-    if(isLogin){
-        return // 나의공간 컴포넌트
-    }else {
+    if (isLogin) {
+        return; // 나의공간 컴포넌트
+    } else {
         return (
-        <div className="MainPage">
-            <BrowserRouter>
-                <div className="mainContainer">
-                    {/* 내공간에서 모든 기능을 제외하고 3D 배경만을 가져와?*/}
-                    {/* 아니면 적절한 이미지 내지 짧은 영상을 배경으로?*/}
-                    <h2>별무리</h2>
-                    <h3>밤하늘의 별을 보며 쓰는 왕멋진일기</h3>
-                    <Routes>
-                        <Route path="/ChangeInfo" element={<ChangeInfo/>}/>
-                        <Route path="/StarList" element={<StarList/>}/>
-                    </Routes>
-                </div>
-                <div className="sideContainer">
-                    {/* <Login/>
+            <div className="MainPage">
+                <BrowserRouter>
+                    <div className="mainContainer">
+                        {/* 내공간에서 모든 기능을 제외하고 3D 배경만을 가져와?*/}
+                        {/* 아니면 적절한 이미지 내지 짧은 영상을 배경으로?*/}
+                        <h2>별무리</h2>
+                        <h3>밤하늘의 별을 보며 쓰는 왕멋진일기</h3>
+                        <Routes>
+                            <Route
+                                path="/ChangeInfo"
+                                element={<ChangeInfo />}
+                            />
+                            <Route path="/StarList" element={<StarList />} />
+                        </Routes>
+                    </div>
+                    <div className="sideContainer">
+                        {/* <Login/>
                     <Regist/> */}
-                    <Sidebar/>  
-                </div> 
-            </BrowserRouter>
-        </div>
-    )
+                        <Sidebar />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
     }
 }
