5807e3d03cf74f006bbabc3e8a59f95984ce6c3d
조한빈 <whgksqls9999@naver.com>
2024-01-26 08:40:21 +0900

    Refactor: modal 더미 데이터 코드 수정

--- FE/src/App.js
+++ FE/src/App.js
@@ -4,11 +4,19 @@ import StarDetail from "./components/star/StarDetail";
 import ReportDetail from "./components/admin/ReportDetail";
 
 function App() {
+  const reportInfo = {
+    reportIndex: 1,
+    boardIndex: 1,
+    userNickname: "유저닉네임",
+    reportContent: "신고 내용",
+    reportInputDate: "신고 날짜",
+  };
+
   return (
     <div className="App">
       <RadioModal />
       <StarDetail starIndex={1} />
-      <ReportDetail /*reportInfo={}*/ />
+      <ReportDetail reportInfo={reportInfo} />
     </div>
   );
 }
--- FE/src/components/Admin/ReportDetail.js
+++ FE/src/components/Admin/ReportDetail.js
@@ -5,19 +5,9 @@ import Modal from "../reusable/Modal";
  * reportInfo : ReportListResponseDto
  * @returns
  */
-function ReportDetail(
-  {
-    /* reportInfo */
-  }
-) {
+function ReportDetail({ reportInfo }) {
   // reportInfo : ReportListResponseDto
-  const reportInfo = {
-    reportIndex: 1,
-    boardIndex: 1,
-    userNickname: "유저닉네임",
-    reportContent: "신고 내용",
-    reportInputDate: "신고 날짜",
-  };
+
   return <Modal type={"report"} starIndex={reportInfo.boardIndex} reportInfo={reportInfo} />;
 }
 
