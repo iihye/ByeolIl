4274e470dd6a58894803d1b9c7c9e45d0503a38e
euny0ung <hdtv0218@gmail.com>
2024-01-25 16:18:46 +0900

    Feat: 일반 로그인 구현 및 API 연동
    Change-Id: I1a008990174eba7fe78af50deb1dd4d42c6fe485

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,7 +1,11 @@
-import FindUser from './components/user/FindUser';
+import Login from './components/login/Login';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Login />
+        </div>
+    );
 }
 
 export default App;
