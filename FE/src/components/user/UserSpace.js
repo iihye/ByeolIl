6dc71f194cc48bb6047be1a479609f5c843f46f2
조한빈 <whgksqls9999@naver.com>
2024-01-25 13:32:28 +0900

    Refactor: 별자리 라인 렌더링 방식 개선
    Change-Id: I08f9e0b87dcd7df0937c24479f1916fd20724b88

--- FE/src/components/user/UserSpace.js
+++ FE/src/components/user/UserSpace.js
@@ -89,8 +89,7 @@ stars.forEach((val) => isAddedStar.set(val.boardLocation, val));
 
 // 별자리 이어짐 체크
 const constellationCheck = (starNum) => {
-  // starNum이 한 페이지 내 별 개수보다 커질 경우도 생각
-  let curStarNum = starNum % totalStarCount;
+  const curStarNum = starNum % totalStarCount;
   let res = true;
 
   if (curStarNum < 4) {
@@ -112,13 +111,16 @@ const constellationCheck = (starNum) => {
 };
 
 function Line(props) {
+  console.log("LINE MOUNTED");
+
   const ref = useRef(null);
   const lineGeometry = new THREE.BufferGeometry().setFromPoints(props.points);
 
   return (
     <>
       <line ref={ref} geometry={lineGeometry}>
-        <lineBasicMaterial attach="material" color={"white"} linewidth={10} />
+        {/* <lineBasicMaterial attach="material" color={0x93492f} transparent={true} opacity={0.2} /> */}
+        <lineBasicMaterial attach="material" transparent={props.lineColor} opacity={0} />{" "}
       </line>
     </>
   );
@@ -138,6 +140,8 @@ function Sphere(props) {
 }
 
 function Star(props) {
+  console.log(`STAR ${props.location} MOUNTED`);
+
   const mesh = useRef(null);
   const [curStarState, setCurStarState] = useState(isAddedStar.get(props.location));
   const colors = {
@@ -146,13 +150,11 @@ function Star(props) {
   };
 
   const updateStarState = () => {
-    // 요 페이지에서는 사실 눌렀을 때 색 변화를 일으키는 게 아니라, 상세보기 / 글쓰기 등 모달창만 띄워주면 됨
-    // 아래는 테스트 코드,, 등록 / 삭제에 따른 색 변화는 글쓰기, 글삭제 기능 추가 후 연동해서 작성해야 할 듯
     // 등록된 별이 아닐 때
     if (!curStarState) {
       if (window.confirm("별을 등록할까요?")) {
         // axios : 별 등록 요청
-        // {현재 별 페이지 번호} * {한 페이지당 별 개수} + {현재 별의 위치 번호} -> 별의 location 값
+        // {현재 별 페이지 번호 - 1} * {한 페이지당 별 개수} + {현재 별의 위치 번호} -> 별의 location 값
         // 별 등록이 성공적으로 이루어졌다면 별 상태를 바꾼다 -> 요건 추후에 API에 맞춰서 수정
         setCurStarState({ boardAccess: "OPEN" });
 
@@ -171,20 +173,9 @@ function Star(props) {
 
         isAddedStar.set(props.location, starData);
 
-        // 별 등록 후, 별자리 이어짐 체크 및 별자리 그리기
+        // 별자리가 완성됐을 때, 선 보이게 하기
         if (constellationCheck(props.location)) {
-          for (let i = 0; i < starTail.length; i++) {
-            if (props.location <= starTail[i]) {
-              let points = [];
-
-              props.positions.forEach((element, index) => {
-                points.push(new THREE.Vector3(...element.slice(1, 4)));
-              });
-
-              props.setLineState(points);
-              break;
-            }
-          }
+          props.setLineColor(false);
         }
       }
 
@@ -200,12 +191,7 @@ function Star(props) {
       } else if (curStarState.boardAccess === "CLOSE") {
         if (window.confirm("비공개 별입니다 \n 별을 삭제할까요?")) {
           // axios : 별 정보 수정 요청 (별 삭제 처리)
-          for (let i = 0; i < starTail.length; i++) {
-            if (props.location <= starTail[i]) {
-              props.setLineState([]);
-              break;
-            }
-          }
+          props.setLineColor(true);
           isAddedStar.delete(props.location);
           setCurStarState(null);
         }
@@ -228,85 +214,71 @@ function Star(props) {
 }
 
 function GroupStar(props) {
+  console.log(`GROUP-STAR ${props.groupNum} MOUNTED`);
+
   const [lineState, setLineState] = useState([]);
-  const position = props.position;
+  const [lineColor, setLineColor] = useState(true);
+
+  const group = useRef(null);
 
   useEffect(() => {
-    const lastStarOfThisGroup = position[position.length - 1][0];
+    setLineState(props.position.map((val) => new THREE.Vector3(...val.slice(1, 4))));
+
+    const lastStarOfThisGroup = props.position[props.position.length - 1][0];
     if (constellationCheck(lastStarOfThisGroup)) {
-      let points = [];
-      position.forEach((element, index) => {
-        points.push(new THREE.Vector3(...element.slice(1, 4)));
-      });
-      setLineState(points);
+      setLineColor(false);
     }
   }, []);
 
+  useFrame((state, delta) => {
+    group.current.rotation.y += delta / 300;
+  });
+
   return (
     <>
-      {position.map((val, index) => (
-        <Star key={index} size={[0.2, 32, 32]} positions={position} position={val.slice(1, 4)} location={val[0]} lineState={lineState} setLineState={setLineState} />
-      ))}
-      <Line points={lineState} />
+      <group
+        ref={group}
+        onPointerOver={() => {
+          console.log(`Over ${props.groupNum}`);
+        }}
+      >
+        {props.position.map((val, index) => (
+          <Star
+            key={index}
+            size={[0.2, 32, 32]}
+            positions={position}
+            position={val.slice(1, 4)}
+            location={val[0 /* + (curPage * 최대 별 개수)*/]}
+            lineState={lineState}
+            setLineState={setLineState}
+            setLineColor={setLineColor}
+          />
+        ))}
+        <Line points={lineState} lineColor={lineColor} />
+      </group>
     </>
   );
 }
 
 function SceneStars() {
-  console.log("SCENE MOUNTED");
-
-  // useEffect(() => {
-  //   let cur = 0;
-  //   let tmp = [];
-  //   starTail.forEach((val, index) => {
-  //     if (constellationCheck(val)) {
-  //       let points = [];
-  //       for (let i = cur; i <= val; i++) {
-  //         points.push(new THREE.Vector3(...position[i]));
-  //       }
-  //       cur = val + 1;
-  //       tmp.push(points);
-  //     }
-  //   });
-  //   setLineState(tmp);
-  // }, []);
+  console.log("SCENE-STARS MOUNTED");
 
   return (
     <>
-      {/* 별
-      {/* index : 별 location 값이랑 동일 */}
-      {/* {position.map((i, index) => {
-        return (
-          <Star
-            size={[0.2, 32, 32]}
-            position={i}
-            location={index % position.length}
-            key={index}
-            constellationCheck={constellationCheck}
-            isAddedStar={isAddedStar}
-            setLineState={setLineState}
-            lineState={lineState}
-          />
-        );
-      })} */}
-
-      {/* 별자리 선 */}
-      {/* {lineState.map((_, index) => {
-        return <Line points={lineState[index]} key={index} />;
-      })}  */}
       {position.map((val, index) => {
-        return <GroupStar position={val} />;
+        return <GroupStar key={index} groupNum={index} position={val} />;
       })}
     </>
   );
 }
 
 function SceneLights() {
+  console.log("SCENE-LIGHTS MOUNTED");
   return (
     <>
       {/* 광원 */}
-      <directionalLight position={[0, 0, 2]} intensity={3} />
-      <ambientLight intensity={1} />
+      <directionalLight position={[0, 0, 5]} intensity={2} />
+      <ambientLight intensity={2} />
 
       {/* 중심점 - 추후 삭제 */}
       <Sphere size={[0.01, 16, 16]} position={[0.5, -7.8, -1]} color={"red"} />
@@ -326,15 +298,16 @@ function SceneEnvironment() {
     </>
   );
 }
+
 function UserSpace() {
   return (
     <>
-      <div id="canvas-container" style={{ height: "100vh", width: "100vw" }}>
+      <div id="canvas-container" style={{ height: "97vh", width: "97vw" }}>
         <Canvas>
           <SceneStars />
           <SceneLights />
           <SceneEnvironment />
-          <OrbitControls dampingFactor={0.12} target={[0, 0, 0]} rotateSpeed={-0.15} />
+          <OrbitControls dampingFactor={0.15} target={[0, 0, 0]} rotateSpeed={-0.15} enableZoom={false} />
           <PerspectiveCamera makeDefault position={[-0.01, 0, 0.1]} fov={60} zoom={1} aspect={window.innerWidth / window.innerHeight} />
         </Canvas>
       </div>
