b0ce319aacabf21da6aedc5e54d0c8eccbfce42e
kdh19583 <kdh19583@gmail.com>
2024-01-25 15:28:58 +0900

    Refactor: App.js, MainPage, Sidebar 변경된 라우터구조 반영
    Change-Id: I43f089584702915562df25b863c23a1d39108626

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,10 +1,17 @@
 import MainPage from "./pages/MainPage";
-import { RecoilRoot } from "recoil";
+import LandingPage from "./pages/LandingPage"
+
+localStorage.setItem('isLogin', true); // 테스트용
 
 function App() {
-  return <div className="App">
-      <MainPage/>
-  </div>;
+  const isLogin = localStorage.getItem('isLogin');
+
+  return (
+    <div className="App">
+      
+      {isLogin ? <MainPage/> : <LandingPage/>}
+    </div>
+  )
 }
 
 export default App;
