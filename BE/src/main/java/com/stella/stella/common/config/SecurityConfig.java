04f9bf46c9c44f14f973e53676d4164dfd167534
이혜진 <floral_berry@naver.com>
2024-01-26 12:45:42 +0900

    Fix: Security 수정
    Change-Id: Idc33ab9952577c6138726be3dda5cab924f8f155

--- BE/src/main/java/com/stella/stella/common/config/SecurityConfig.java
+++ BE/src/main/java/com/stella/stella/common/config/SecurityConfig.java
@@ -28,49 +28,48 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-
-				.authorizeRequests(authorizeRequests ->
-						authorizeRequests
-								.requestMatchers("/**")
-								.permitAll()
-//								.requestMatchers("/member/login/**").permitAll()
-//								.requestMatchers("/member/join/**").permitAll()
-//								.requestMatchers("/follow/**").permitAll()
-//								.requestMatchers("/member/dup-check/**").permitAll()
-//								.requestMatchers("/member/test").hasRole("USER")
-//								.requestMatchers("/member/check/email").permitAll()
-//								.requestMatchers("/member/find/**").permitAll()
-//								.requestMatchers("/member/ban").hasRole("ADMIN")
-//								.requestMatchers("/member/search/list").permitAll()
-//                				.requestMatchers("/member/test").hasAnyRole("USER","ADMIN")
-//								.anyRequest().authenticated()
-
-				)
-				.addFilterBefore(
-						new JwtAuthenticationFilter(jwtTokenProvider, memberRepository, authenticationManagerBuilder),
-						UsernamePasswordAuthenticationFilter.class)
-				.csrf().disable()
-				.headers().frameOptions().disable()
-				.and()
-				.cors(); // CORS 활성화
-
-//		http.httpBasic().disable().csrf().disable().sessionManagement()
-//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-//				.requestMatchers("/member/login/**").permitAll()
-//				.requestMatchers("/member/join/**").permitAll()
-//				.requestMatchers("/follow/**").permitAll()
-//				.requestMatchers("/member/dup-check/**").permitAll()
-//				.requestMatchers("/member/test").hasRole("USER")
-//				.requestMatchers("/member/check/email").permitAll()
-//				.requestMatchers("/member/find/**").permitAll()
-//				.requestMatchers("/member/ban").hasRole("ADMIN")
-//				.requestMatchers("/member/search/list").permitAll()
-////            .requestMatchers("/member/test").hasAnyRole("USER","ADMIN")
-//				.anyRequest().authenticated().and().addFilterBefore(
+//		http
+//				.authorizeRequests(authorizeRequests ->
+//						authorizeRequests
+//								.requestMatchers("/**")
+//								.permitAll()
+////								.requestMatchers("/member/login/**").permitAll()
+////								.requestMatchers("/member/join/**").permitAll()
+////								.requestMatchers("/follow/**").permitAll()
+////								.requestMatchers("/member/dup-check/**").permitAll()
+////								.requestMatchers("/member/test").hasRole("USER")
+////								.requestMatchers("/member/check/email").permitAll()
+////								.requestMatchers("/member/find/**").permitAll()
+////								.requestMatchers("/member/ban").hasRole("ADMIN")
+////								.requestMatchers("/member/search/list").permitAll()
+////                				.requestMatchers("/member/test").hasAnyRole("USER","ADMIN")
+////								.anyRequest().authenticated()
+//
+//				)
+//				.addFilterBefore(
 //						new JwtAuthenticationFilter(jwtTokenProvider, memberRepository, authenticationManagerBuilder),
 //						UsernamePasswordAuthenticationFilter.class)
-//				.cors();
+//				.csrf().disable()
+//				.headers().frameOptions().disable()
+//				.and()
+//				.cors(); // CORS 활성화
+
+		http.httpBasic().disable().csrf().disable().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.requestMatchers("/member/login/**").permitAll()
+				.requestMatchers("/member/join/**").permitAll()
+				.requestMatchers("/follow/**").permitAll()
+				.requestMatchers("/member/dup-check/**").permitAll()
+				.requestMatchers("/member/test").hasRole("USER")
+				.requestMatchers("/member/check/email").permitAll()
+				.requestMatchers("/member/find/**").permitAll()
+				.requestMatchers("/member/ban").hasRole("ADMIN")
+				.requestMatchers("/member/search/list").permitAll()
+//            .requestMatchers("/member/test").hasAnyRole("USER","ADMIN")
+				.anyRequest().authenticated().and().addFilterBefore(
+						new JwtAuthenticationFilter(jwtTokenProvider, memberRepository, authenticationManagerBuilder),
+						UsernamePasswordAuthenticationFilter.class)
+				.cors();
 		return http.build();
 	}
 
