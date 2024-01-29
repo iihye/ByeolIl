7d5bf6fb03c75967863f059b77314c84b1e71246
조한빈 <whgksqls9999@naver.com>
2024-01-29 17:06:27 +0900

    Refactor: 별자리 체크 코드 개선
    Change-Id: I5b3b573d017259bec6ea406453a89f13ed1ac623

--- FE/src/components/user/UserSpace.js
+++ FE/src/components/user/UserSpace.js
@@ -1,20 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, useTexture } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 import * as THREE from "three";
 import axios from "axios";
 import { atom, useRecoilState } from "recoil";
 
-// starRange : 각 별자리들의 시작별, 끝 별 번호
-const starRange = [
-  [0, 4],
-  [5, 6],
-];
-
-// position : 변화할 일 없는 값
+// position : 별 [번호, x, y, z]
 const position = [
   [
+    // 작은곰자리 URSA MINOR
     [0, 0, 22, 0],
     [1, -3.1, 21.9, -0.7],
     [2, -6, 21.5, -2.1],
@@ -24,6 +20,7 @@ const position = [
     [6, -8.6, 20, -9.2],
   ],
   [
+    // 기린자리 CAMELOPARDALIS\
     [7, 7.1, 21, 5.5],
     [8, 11.3, 18.8, 10.8],
     [9, 10.2, 20.5, 3.2],
@@ -32,6 +29,7 @@ const position = [
     [12, 18.8, 17, 5],
   ],
   [
+    // 세페우스자리 CEPHEUS
     [13, -1.3, 20.7, 10.2],
     [14, -6.2, 19.4, 12.9],
     [15, -2.5, 18.5, 16.1],
@@ -41,6 +39,7 @@ const position = [
     [19, -5.5, 16.3, 19.9],
   ],
   [
+    // 카시오페이아자리 CASSIOPEIA
     [20, 3, 18.1, 17],
     [21, 5, 16.8, 19],
     [22, 7, 16.5, 19],
@@ -51,6 +50,7 @@ const position = [
     [27, 0.6, 15.9, 21.3],
   ],
   [
+    // 안드로메다자리 ANDROMEDA
     [28, 6.5, 11.8, 25.9],
     [29, 6.3, 6.5, 31],
     [30, -0.6, 10.9, 27.7],
@@ -63,12 +63,14 @@ const position = [
     [37, 12.2, 9.4, 26.5],
   ],
   [
+    // 삼각형자리 TRIANGULUM
     [38, 20.4, 0.2, 29.2],
     [39, 21.6, -4.9, 31],
     [40, 23.1, -4, 29.5],
     [41, 22.4, -1.1, 28.5],
   ],
   [
+    // 페르세우스자리 PERSEUS
     [42, 20.8, 7.4, 22.8],
     [43, 20.4, 8.7, 21.7],
     [44, 19.2, 10.6, 20.3],
@@ -87,6 +89,7 @@ const position = [
     [57, 20.1, 14, 13.2],
   ],
   [
+    // 마차부자리 AURIGA
     [58, 27.1, 10.9, 5.6],
     [59, 29.5, 8.2, 6.5],
     [60, 29.5, 7.5, 9],
@@ -99,6 +102,7 @@ const position = [
     [67, 35.8, -1.4, 6.5],
   ],
   [
+    // 살쾡이자리 LINX
     [68, 25.2, 13.1, -0.8],
     [69, 25.1, 12.9, -3.9],
     [70, 30.3, 7.5, -5.8],
@@ -109,6 +113,7 @@ const position = [
     [75, 30.3, -6.2, -23.3],
   ],
   [
+    // 도마뱀자리 LACERTA
     [76, -14, 0.3, 32.7],
     [77, -11, 3.3, 32],
     [78, -11.5, 7.2, 28.9],
@@ -118,6 +123,7 @@ const position = [
     [82, -9.2, 12.2, 24.6],
   ],
   [
+    // 백조자리 CYGNUS
     [83, -20.6, 0.5, 28.9],
     [84, -22.2, 2.5, 26.2],
     [85, -21.4, 7.6, 22.1],
@@ -128,13 +134,16 @@ const position = [
     [90, -27.9, 0.7, 21.7],
   ],
   [
+    // 거문고자리 LYRA
     [91, -35.1, 0.2, 6.2],
     [92, -35.1, 0.9, 3],
     [93, -31.9, 5.6, 5],
     [94, -31.9, 6, 2.21],
     [95, -30.7, 7.6, 0.4],
   ],
+
   [
+    // 헤라클레스자리 HERACULES
     [96, -32.5, 3.3, -9.4],
     [97, -27.9, 9.7, -7.4],
     [98, -29.9, 5.2, -12.9],
@@ -144,6 +153,7 @@ const position = [
     [102, -33.4, -1.1, -14.1],
   ],
   [
+    // 용자리 DRACO
     [103, -24.1, 13.5, -5.4],
     [104, -20.9, 15.6, -5.7],
     [105, -21.9, 14.7, -7.1],
@@ -163,6 +173,7 @@ const position = [
     [119, -0.7, 18.7, -15.9],
   ],
   [
+    // 목동자리 BOOTES
     [120, -23.7, 2.7, -24.7],
     [121, -21.9, 0.8, -27.7],
     [122, -17.4, 6.1, -26.8],
@@ -174,10 +185,12 @@ const position = [
     [128, -24.9, -8.1, -29.7],
   ],
   [
+    // 사냥개자리 CANES VENATICI
     [129, -5, 2.5, -34],
     [130, -0.2, 3.1, -34],
   ],
   [
+    // 큰곰자리 URSA MAJOR
     [131, -11.4, 12.2, -23.6],
     [132, -6.6, 15, -21.6],
     [133, -3.8, 15.3, -21.8],
@@ -201,221 +214,23 @@ const position = [
   ],
 ];
 
-// const position = [
-//   [
-//     // URAS
-//     [0, 0, 40, 0],
-//     [1, -3.1, 39.9, -0.7],
-//     [2, -6, 39.5, -2.1],
-//     [3, -8.4, 38.7, -5.7],
-//     [4, -10.5, 38.3, -5.1],
-//     [5, -11.4, 37.3, -8.9],
-//     [6, -8.6, 38, -9.2],
-//   ],
-//   [
-//     // CAMELOPARDALTS
-//     [7, 7.1, 39, 5.5],
-//     [8, 11.3, 36.8, 10.8],
-//     [9, 10.2, 38.5, 3.2],
-//     [10, 10.4, 38.6, -2],
-//     [11, 15.5, 36.8, 3],
-//     [12, 18.8, 35, 5],
-//   ],
-//   [
-//     //CEPHEUS
-//     [13, -1.3, 38.7, 10.2],
-//     [14, -6.2, 37.4, 12.9],
-//     [15, -2.5, 36.5, 16.1],
-//     [16, -7.1, 34.3, 19.4],
-//     [17, -9, 35.2, 16.7],
-//     [18, -8.1, 33.3, 20.6],
-//     [19, -5.5, 34.3, 19.9],
-//   ],
-//   [
-//     //CASSIOPEIA
-//     [20, 3, 36.1, 17],
-//     [21, 5, 34.8, 19],
-//     [22, 7, 34.5, 19],
-//     [23, 8, 35.5, 16.7],
-//     [24, 4.4, 33.8, 21],
-//     [25, 2.9, 33.1, 22.2],
-//     [26, 3.9, 31.8, 24],
-//     [27, 0.6, 33.9, 21.3],
-//   ],
-//   [
-//     // ANDROMEDA
-//     [28, 6.5, 29.8, 25.9],
-//     [29, 6.3, 24.5, 31],
-//     [30, -0.6, 28.9, 27.7],
-//     [31, -4.6, 26.7, 29.4],
-//     [32, 5, 20.6, 33.9],
-//     [33, 2.3, 22.6, 32.9],
-//     [34, 2, 17.7, 35.8],
-//     [35, 8.5, 15.2, 36],
-//     [36, 10.3, 21.1, 32.4],
-//     [37, 12.2, 27.4, 26.5],
-//   ],
-//   [
-//     // TRIANGULUM
-//     [38, 20.4, 18.2, 29.2],
-//     [39, 21.6, 13.1, 31],
-//     [40, 23.1, 14, 29.5],
-//     [41, 22.4, 16.9, 28.5],
-//   ],
-//   [
-//     // PERSEUS
-//     [42, 20.8, 25.4, 22.8],
-//     [43, 20.4, 26.7, 21.7],
-//     [44, 19.2, 28.6, 20.3],
-//     [45, 17.5, 30.7, 18.7],
-//     [46, 15.4, 31.9, 18.5],
-//     [47, 14.1, 33.3, 17.1],
-//     [48, 16.4, 32.1, 17.4],
-//     [49, 18.8, 30.8, 17.2],
-//     [50, 20.4, 30.4, 16.1],
-//     [51, 23.5, 27.7, 16.8],
-//     [52, 26.2, 25.8, 15.7],
-//     [53, 28, 23.5, 16.3],
-//     [54, 29.5, 20.2, 17.9],
-//     [55, 21.7, 30.3, 14.5],
-//     [56, 21.9, 30.7, 13.4],
-//     [57, 20.1, 32, 13.2],
-//   ],
-//   [
-//     // AURIGA
-//     [58, 27.1, 28.9, 5.6],
-//     [59, 29.5, 26.2, 6.5],
-//     [60, 29.5, 25.5, 9],
-//     [61, 28, 27.3, 8.5],
-//     [62, 32.8, 20.8, 9.5],
-//     [63, 35.1, 17.4, 8],
-//     [64, 23.5, 32.3, 2],
-//     [65, 28.1, 28.4, 1.9],
-//     [66, 32.1, 23.8, 2.1],
-//     [67, 35.8, 16.6, 6.5],
-//   ],
-//   [
-//     // LINX
-//     [68, 25.2, 31.1, -0.8],
-//     [69, 25.1, 30.9, -3.9],
-//     [70, 30.3, 25.5, -5.8],
-//     [71, 31.2, 21.1, -13.5],
-//     [72, 29.3, 20.3, -18.1],
-//     [73, 31, 16.1, -19.5],
-//     [74, 29.5, 16.1, -21.7],
-//     [75, 30.3, 11.8, -23.3],
-//   ],
-//   [
-//     // LACERTA
-//     [76, -14, 18.3, 32.7],
-//     [77, -11, 21.3, 32],
-//     [78, -11.5, 25.2, 28.9],
-//     [79, -10, 26.1, 28.6],
-//     [80, -10.3, 27.5, 27.2],
-//     [81, -8.7, 28.6, 26.6],
-//     [82, -9.2, 30.2, 24.6],
-//   ],
-//   [
-//     //CYGNUS
-//     [83, -20.6, 18.5, 28.9],
-//     [84, -22.2, 20.5, 26.2],
-//     [85, -21.4, 25.6, 22.1],
-//     [86, -17.6, 28.6, 21.7],
-//     [87, -22.2, 28, 18],
-//     [88, -21, 30.9, 14.3],
-//     [89, -24.7, 22.3, 22.2],
-//     [90, -27.9, 18.7, 21.7],
-//   ],
-//   [
-//     // LIRA
-//     [91, -35.1, 18.2, 6.2],
-//     [92, -35.1, 18.9, 3],
-//     [93, -31.9, 23.6, 5],
-//     [94, -31.9, 24, 2.21],
-//     [95, -30.7, 25.6, 0.4],
-//   ],
-//   [
-//     // HERACULES
-//     [96, -32.5, 21.3, -9.4],
-//     [97, -27.9, 27.7, -7.4],
-//     [98, -29.9, 23.2, -12.9],
-//     [99, -27.2, 25.9, -13.8],
-//     [100, -24.4, 28.5, -13.9],
-//     [101, -34.8, 15.6, -12.1],
-//     [102, -33.4, 16.9, -14.1],
-//   ],
-//   [
-//     // DRACO
-//     [103, -24.1, 31.5, -5.4],
-//     [104, -20.9, 33.6, -5.7],
-//     [105, -21.9, 32.7, -7.1],
-//     [106, -23.6, 31.4, -7.5],
-//     [107, -20.6, 34.2, -2],
-//     [108, -16.7, 36.3, -1.6],
-//     [109, -15, 37.1, -0.2],
-//     [110, -13.3, 37.7, -2.2],
-//     [111, -13.9, 37.3, -3.8],
-//     [112, -16.3, 35.8, -7.4],
-//     [113, -16.9, 34.7, -10.4],
-//     [114, -16.9, 34.6, -10.7],
-//     [115, -17.4, 33.6, -13],
-//     [116, -14.9, 34, -15],
-//     [117, -8.8, 35.1, -17.1],
-//     [118, -3.2, 36.7, -15.7],
-//     [119, -0.7, 36.7, -15.9],
-//   ],
-//   [
-//     // BOOTES
-//     [120, -23.7, 20.7, -24.7],
-//     [121, -21.9, 18.8, -27.7],
-//     [122, -17.4, 24.1, -26.8],
-//     [123, -15.1, 27.6, -24.7],
-//     [124, -16.9, 27.9, -23.1],
-//     [125, -27.7, 14.1, -25.2],
-//     [126, -28, 7.7, -27.5],
-//     [127, -30.5, 11.3, -23.3],
-//     [128, -24.9, 9.9, -29.7],
-//   ],
-//   [
-//     // CANES VENATICI
-//     [129, -5, 20.5, -34],
-//     [130, -0.2, 21.1, -34],
-//   ],
-//   [
-//     // URSA MAJOR
-//     [131, -11.4, 30.2, -23.6],
-//     [132, -6.6, 33, -21.6],
-//     [133, -3.8, 33.3, -21.8],
-//     [134, -0.4, 33.7, -21.5],
-//     [135, -1.3, 32.3, -23.5],
-//     [136, 6.5, 32.6, -22.3],
-//     [137, 5.1, 35.1, -18.5],
-//     [138, 9.7, 35.3, -16.1],
-//     [139, 14.2, 34.5, -14.5],
-//     [140, 9.8, 33.8, -19],
-//     [141, 11.7, 32.2, -20.6],
-//     [142, 14.3, 31.1, -20.7],
-//     [143, 17.6, 29.1, -21.1],
-//     [144, 17.7, 29.8, -19.9],
-//     [145, 2.2, 30, -26.4],
-//     [146, 6.4, 27.9, -27.9],
-//     [147, 12, 27.7, -26.2],
-//     [148, 11.8, 25.7, -28.3],
-//     [149, 6.1, 21.3, -33.3],
-//     [150, 6, 18.6, -34.9],
-//   ],
-// ];
+// 해당 별자리 내 첫 번째 별 번호, 마지막 별 번호
+const starRange = [];
+position.forEach((element, index) => starRange.push([element[0][0], element[element.length - 1][0]]));
+console.log(starRange);
+// 페이지 내 존재하는 별 개수
+const totalStarCount = position[position.length - 1][position[position.length - 1].length - 1][0] + 1;
 
-const totalStarCount = 129;
-
-const starsState = atom({
-  key: "stars",
-  default: [],
-});
-
+// 현재 페이지
 const curPageState = atom({
   key: "curPage",
   default: 0,
+});
+
+// 현재 페이지에서 작성된 별 목록
+const starsState = atom({
+  key: "stars",
+  default: [],
 });
 
 const tmpStars = [
@@ -482,37 +297,22 @@ const isAddedStar = new Map();
 
 // 별자리 이어짐 체크
 const constellationCheck = (starNum) => {
-  const curStarNum = starNum % totalStarCount;
   let res = true;
 
-  if (curStarNum < 5) {
-    for (let i = 0; i < 5; i++) {
-      if (!isAddedStar.has(i)) {
-        res = false;
-        break;
+  outer: for (let i = 0; i < starRange.length; i++) {
+    if (starRange[i][1] >= starNum) {
+      for (let j = starRange[i][0]; j <= starRange[i][1]; j++) {
+        if (!isAddedStar.has(j)) {
+          res = false;
+          break outer;
+        }
       }
-    }
-  } else if (curStarNum < 7) {
-    for (let i = 5; i < 7; i++) {
-      if (!isAddedStar.has(i)) {
-        res = false;
-        break;
-      }
+      break;
     }
   }
+
   return res;
 };
-
-function Cube(props) {
-  return (
-    <>
-      <mesh>
-        <boxGeometry args={[100, 100, 100]} />
-        <meshStandardMaterial color={"black"} side={THREE.DoubleSide} />
-      </mesh>
-    </>
-  );
-}
 
 function Line(props) {
   console.log("LINE MOUNTED");
@@ -531,6 +331,9 @@ function Line(props) {
 function Sphere(props) {
   console.log("SPHERE MOUNTED");
 
+  const texture = useTexture("texture/bakedUniverse.png");
+  console.log(texture);
+
   const mesh = useRef(null);
 
   return (
@@ -542,7 +345,7 @@ function Sphere(props) {
 }
 
 function Star(props) {
-  console.log(`STAR ${props.location} MOUNTED`);
+  // console.log(`STAR ${props.location} MOUNTED`);
 
   const mesh = useRef(null);
   const [stars, setStars] = useRecoilState(starsState);
@@ -629,6 +432,54 @@ function GroupStar(props) {
   const [lineColor, setLineColor] = useState(true);
   const group = useRef(null);
 
+  // 별자리 생성 체크용 트리
+  const range = starRange[props.groupNum];
+  const size = 1 << (Math.ceil(Math.log(range[1] - range[0] + 1) / Math.log(2)) + 1);
+  const constellationTree = Array(size).fill(true);
+
+  const initTree = (node, start, end) => {
+    if (start === end) {
+      constellationTree[node] = isAddedStar.get(range[0] + start) ? true : false;
+      return;
+    }
+
+    let mid = Math.floor((start + end) / 2);
+    initTree(node * 2, start, mid);
+    initTree(node * 2 + 1, mid + 1, end);
+    constellationTree[node] = constellationTree[node * 2] && constellationTree[node * 2 + 1];
+  };
+
+  const updateTree = (node, start, end, idx, val) => {
+    if (idx < start || end < idx) {
+      return;
+    }
+
+    if (start === end) {
+      constellationTree[node] = val;
+      return;
+    }
+
+    let mid = Math.floor((start + end) / 2);
+    updateTree(node * 2, start, mid, idx, val);
+    updateTree(node * 2 + 1, mid + 1, end, idx, val);
+    constellationTree[node] = constellationTree[node * 2] && constellationTree[node * 2 + 1];
+  };
+
+  const queryTree = (node, start, end, left, right) => {
+    if (end < left || right < start) {
+      return true;
+    }
+
+    if (left <= start && end <= right) {
+      return constellationTree[node];
+    }
+
+    let mid = Math.floor((start + end) / 2);
+    let l = queryTree(node * 2, start, mid, left, right);
+    let r = queryTree(node * 2 + 1, mid + 1, end, left, right);
+    return l && r;
+  };
+
   useEffect(() => {
     setLineState(props.position.map((val) => new THREE.Vector3(...val.slice(1, 4))));
 
@@ -639,7 +490,7 @@ function GroupStar(props) {
   }, [stars]);
 
   useFrame((state, delta) => {
-    group.current.rotation.y += delta / 300;
+    group.current.rotation.y += delta / 220;
   });
 
   return (
@@ -667,6 +518,7 @@ function SceneStars() {
   const REST_API_URI = ""; // env 설정
   const member_index = ""; // 페이지 url에서 파싱
 
+  // 페이지 내 별 정보 불러오기
   useEffect(() => {
     const fetchData = async () => {
       await axios
@@ -677,7 +529,7 @@ function SceneStars() {
           setStars(response.data);
         })
         .catch((e) => {
-          // 임시 데이터
+          // 임시 데이터 - 나중에 제거할 예정
           console.log(e);
           isAddedStar.clear();
           tmpStars.forEach((star) => isAddedStar.set(star.boardLocation, star));
@@ -712,15 +564,16 @@ function SceneLights() {
 }
 
 function SceneEnvironment() {
+  // const texture = useLoader(TextureLoader, "texture/bakedUniverse.png");
+
   return (
     <>
       {/* 우주 배경 */}
-      {/* <Sphere size={[40, 48, 48]} position={[0, -4, 0]} color={"black"} type={"back"} /> */}
-      <Cube />
+      <Sphere size={[40, 48, 48, 0, Math.PI * 2, 0, (Math.PI * 2) / 3]} position={[0, -4, 0]} color={"black"} type={"back"} />
+      {/* <Cube /> */}
 
       {/* 바닥면 */}
       <Sphere size={[2, 48, 48, 0, Math.PI * 2]} position={[0, -2.2, 0]} color={"orange"} />
-      {/* <Sphere size={[2, 48, 48, 0, Math.PI * 2, 0, Math.PI / 2]} position={[0, 0, -10]} color={"orange"} /> */}
     </>
   );
 }
@@ -735,7 +588,7 @@ function UserSpace() {
           <SceneEnvironment />
           <OrbitControls dampingFactor={0.15} target={[0, 0, 0]} rotateSpeed={-0.15} enableZoom={true} />
           <PerspectiveCamera makeDefault position={[-0.01, 0, 0.1]} fov={60} zoom={1} aspect={window.innerWidth / window.innerHeight} />
-          <axesHelper />
+          {/* <axesHelper /> */}
         </Canvas>
       </div>
     </>
