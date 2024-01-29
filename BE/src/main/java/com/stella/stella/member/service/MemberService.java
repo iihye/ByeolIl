3699855bbfc3a92732c352f07236a412930a1bd9
이효재 <orange1th@gmail.com>
2024-01-29 10:13:59 +0900

    Fix: 리다이렉트 url
    Change-Id: Iebb46bdd0f86dc0505b09343b1dd4abc6a48b7b7
               Comments	 2 resolved

--- BE/src/main/java/com/stella/stella/member/service/MemberService.java
+++ BE/src/main/java/com/stella/stella/member/service/MemberService.java
@@ -47,6 +47,9 @@ public class MemberService {
     @Value("${kakao.key}")
     private String kakaoRestAPIKey;
 
+    @Value("@{kakao.url}")
+    private String kakaoRedirectUrl;
+
     @Transactional
     public String login(String memberId, String memberPassword, String memberPlatform) throws Exception{
 
@@ -92,7 +95,7 @@ public class MemberService {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", kakaoRestAPIKey);
-        params.add("redirect_uri", "https://i10b209.p.ssafy.io/" + url);
+        params.add("redirect_uri", kakaoRedirectUrl + url);
 //        params.add("redirect_uri", "http://localhost:8080/" + url);
         params.add("code", code);
         // Set http entity
