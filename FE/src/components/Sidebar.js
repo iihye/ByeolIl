ce5f44be27c77c1f78b3da039ec8114269e1ec2e
euny0ung <hdtv0218@gmail.com>
2024-01-30 12:36:01 +0900

    Chore: 프리티어
    Change-Id: I3e8cd16da32be708fa443d60fd64b05e654f250e

--- FE/src/components/Sidebar.js
+++ FE/src/components/Sidebar.js
@@ -1,26 +1,26 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
 // import StarFavoList from './star/StarFavorList'
 // import FollowList from './user/FollowList'
 // import FindUser from './user/FindUser'
 // import StarTagSearch from './star/StarTagSearch'
 // 환경설정 컴포넌트..?
 // import Alarm from './user/Alarm'
-import {Link} from "react-router-dom"
+import { Link } from 'react-router-dom';
 const items = [
-    { name: "회원정보수정", path:"/ChangeInfo"},
-    { name: "별리스트", path:"/StarList"},
-    { name: "좋아하는별리스트", path:"/StarFavoList"},
+    { name: '회원정보수정', path: '/ChangeInfo' },
+    { name: '별리스트', path: '/StarList' },
+    { name: '좋아하는별리스트', path: '/StarFavoList' },
 ];
 
-function SideBarItem({item}) {
+function SideBarItem({ item }) {
     return (
         <div className="sidebar-item">
-          <p>{item.name}</p>
+            <p>{item.name}</p>
         </div>
-      );
+    );
 }
 
-function SidebarList (props) {
+function SidebarList(props) {
     return (
         <div className="SidebarList">
             <ul>
@@ -28,23 +28,21 @@ function SidebarList (props) {
                 {items.map((item, index) => {
                     return (
                         <Link to={item.path} key={index}>
-                            <SideBarItem
-                                item={item}
-                            />
+                            <SideBarItem item={item} />
                         </Link>
                     );
                 })}
                 <button>로그아웃</button>
             </ul>
         </div>
-    )
+    );
 }
 
-export default function Sidebar (props) {
-    const [viewSideBar ,setViewSideBar] = useState(false);
+export default function Sidebar(props) {
+    const [viewSideBar, setViewSideBar] = useState(false);
 
-    function toggleSidebar () {
-        if(viewSideBar === false) setViewSideBar(true); 
+    function toggleSidebar() {
+        if (viewSideBar === false) setViewSideBar(true);
         else setViewSideBar(false);
     }
 
@@ -52,8 +50,8 @@ export default function Sidebar (props) {
         <div className="Sidebar">
             <button onClick={toggleSidebar}>=</button>
             <div>
-                {viewSideBar ? <SidebarList name={props.name}/> : <div/>}
+                {viewSideBar ? <SidebarList name={props.name} /> : <div />}
             </div>
         </div>
-    )
+    );
 }
