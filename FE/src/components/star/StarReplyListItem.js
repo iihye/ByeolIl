120235908c63ea3b44eeb7f6c54b5de812d00d12
조한빈 <whgksqls9999@naver.com>
2024-01-30 10:27:18 +0900

    Feat: 대댓글 컴포넌트 작성

--- FE/src/components/star/StarReplyListItem.js
+++ FE/src/components/star/StarReplyListItem.js
@@ -1,4 +1,9 @@
+import { useRef, useState } from "react";
+import StarMultiReplyList from "./StarMultiReplyList";
+
 function StarReplyListItem({ reply }) {
+  const [multiReply, setMultiReply] = useState(false);
+
   return (
     <div className="star-reply-list-item" style={{ border: "1px solid black", margin: "5px" }}>
       <div style={{ display: "flex" }}>
@@ -8,9 +13,27 @@ function StarReplyListItem({ reply }) {
       <div>
         <div>{reply.comment_content}</div>
       </div>
-      <div>답글달기</div>
+      <div onClick={() => {setMultiReply(!multiReply)}}>답글달기</div>
+      <StarMultiReplyList/>
+      {
+        multiReply && <MultiReplyInput setMultiReply={setMultiReply}/>
+      }
     </div>
   );
+}
+
+function MultiReplyInput(props){
+  const input = useRef(null);
+  const handleMultiReplyQuit = () => {props.setMultiReply(false)}
+  const handleMultiReplySubmit = () => {}
+
+  return (
+    <div>
+      └ <input ref={input}/>
+      <button onClick={() => {handleMultiReplyQuit()}}>취소</button>
+      <button onClick={() => {handleMultiReplySubmit()}}>등록</button>
+    </div>
+  )
 }
 
 export default StarReplyListItem;
