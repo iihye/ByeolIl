6444540f78768df56249de852b2b22ad37d479f5
euny0ung <hdtv0218@gmail.com>
2024-01-30 11:15:33 +0900

    Chore: 빌드 확인용
    Change-Id: I17b49bbb843705ae874dd254e2d9e9cba5a3a7a7

--- FE/src/components/Header.js
+++ FE/src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div className="Header">
-            <div>고정된 헤더입니다</div>
+            <div>고정된 헤더입니다!!</div>
             <Link to="/landing/login">
                 <button>로그인</button>
             </Link>
