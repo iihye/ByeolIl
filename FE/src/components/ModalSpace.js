1bde758df6321ae411f7099cc9642518a5c9edef
조한빈 <whgksqls9999@naver.com>
2024-02-04 09:33:38 +0900

    Fix: 0번 게시글 모달 안뜨는 버그 수정
    Change-Id: Ibf8fe804267cb00e9bd285d39ab8cb06dfe4c038

--- FE/src/components/ModalSpace.js
+++ FE/src/components/ModalSpace.js
@@ -3,59 +3,31 @@ import { isStarRegistOpenState, isStarModifyOpenState, isStarDetailOpenState, is
 import StarRegist from "./star/StarRegist";
 import StarDetail from "./star/StarDetail";
 
-function ModalSpace(){
-    return(
-        <>
-            <StarRegistArea/>
-            <StarDetailArea/>
-            <StarModifyArea/>
-        </>
-    )
+function ModalSpace() {
+  return (
+    <>
+      <StarRegistArea />
+      <StarDetailArea />
+      <StarModifyArea />
+    </>
+  );
 }
 
-function StarRegistArea(){
-    const isStarRegistOpen = useRecoilValue(isStarRegistOpenState);
-    
-    return (
-        <>
-            {
-                isStarRegistOpen ? (
-                    <StarRegist type={'regist'} location={isStarRegistOpen} /> 
-                ) : null
-            }
+function StarRegistArea() {
+  const isStarRegistOpen = useRecoilValue(isStarRegistOpenState);
 
-        </>
-    );
+  return <>{isStarRegistOpen !== false ? <StarRegist type={"regist"} location={isStarRegistOpen} /> : null}</>;
 }
 
-function StarModifyArea(){
-    const isStarModifyOpen = useRecoilValue(isStarModifyOpenState);
-    return(
-        <>
-             {   
-                isStarModifyOpen ? (
-                    <StarRegist type={'modify'} preBoard={isStarModifyOpen}/>
-                ) : null
-            }
-        </>
-    )
+function StarModifyArea() {
+  const isStarModifyOpen = useRecoilValue(isStarModifyOpenState);
+  return <>{isStarModifyOpen !== false ? <StarRegist type={"modify"} preBoard={isStarModifyOpen} /> : null}</>;
 }
 
-function StarDetailArea(){
-    const isStarDetailOpen = useRecoilValue(isStarDetailOpenState);
+function StarDetailArea() {
+  const isStarDetailOpen = useRecoilValue(isStarDetailOpenState);
 
-    return (
-        <>
-            {isStarDetailOpen && (
-                <StarDetail
-                    starIndex={isStarDetailOpen[0]}
-                    userIndex={isStarDetailOpen[1]}
-                />
-            )}
-        </>
-    );
-
+  return <>{isStarDetailOpen !== false ? <StarDetail starIndex={isStarDetailOpen[0]} userIndex={isStarDetailOpen[1]} /> : null}</>;
 }
-
 
 export default ModalSpace;
