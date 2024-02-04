885daefa0b589ab4bf7567ae309fe0e916a28b48
조한빈 <whgksqls9999@naver.com>
2024-02-04 11:27:34 +0900

    Feat: 댓글 삭제 기능 구현
    Change-Id: Ie3d071d62e0475c5b31200f8ef98aacd20897155

--- FE/src/components/star/StarReplyListItem.js
+++ FE/src/components/star/StarReplyListItem.js
@@ -1,73 +1,119 @@
 import { useRef, useState } from "react";
 import StarMultiReplyList from "./StarMultiReplyList";
 import axios from "axios";
+import { useRecoilState } from "recoil";
+import { renewReplyState } from "components/atom";
 
 function StarReplyListItem(props) {
+  const [renewReply, setRenewReply] = useRecoilState(renewReplyState);
+
   const [multiReply, setMultiReply] = useState(false);
   const [multiReplyList, setMultiReplyList] = useState(props.reply.multiComments);
-  
-  const handleDelete = async () => {
-    /* 댓글 작성자 체크 */  
 
-    // await axios.
-  }
+  const commentIndex = props.reply.commentIndex;
+  const writerIndex = props.reply.memberIndex;
+  const commentRegDate = props.reply.commentRegdate;
+  const commentContent = props.reply.commentContent;
+  const multiComments = props.reply.multiComments;
+  console.log(props);
+  const loginUserIndex = Number(JSON.parse(atob(localStorage.getItem("token").split(" ")[1].split(".")[1])).sub);
+
+  const isWriter = () => {
+    console.log(writerIndex, loginUserIndex);
+    return writerIndex === loginUserIndex;
+  };
+
+  const handleDelete = async () => {
+    const data = {
+      commentIndex: commentIndex,
+      memberIndex: writerIndex,
+    };
+
+    await axios
+      .delete(`${process.env.REACT_APP_API_URL}/comment/`, {
+        header: {
+          token: localStorage.getItem("token"),
+        },
+        data: { ...data },
+      })
+      .then((response) => {
+        if (response.data.map.response === "success") {
+          setRenewReply(!renewReply);
+        }
+      });
+  };
 
   return (
     <div className="star-reply-list-item" style={{ border: "1px solid black", margin: "5px" }}>
       <div style={{ display: "flex" }}>
-        <div>{props.reply.userIndex}번 유저</div>
-        <div>{props.reply.commentRegTime}</div>
+        <div>{commentIndex}번 유저</div>
+        <div>{commentRegDate}</div>
       </div>
-      <div style={{display: "flex"}}>
-        <div>{props.reply.commentContent}</div>
-        <div>
-          <button onClick={handleDelete}>댓글 삭제</button>
+      <div style={{ display: "flex" }}>
+        <div>{commentContent}</div>
+        <div>{isWriter() ? <button onClick={handleDelete}>댓글 삭제</button> : null}</div>
+      </div>
+      {!multiReply && (
+        <div
+          onClick={() => {
+            setMultiReply(true);
+          }}
+        >
+          답글달기
         </div>
-      </div>
-      {
-        !multiReply && <div onClick={() => {setMultiReply(true)}}>답글달기</div>
-      }
-      <StarMultiReplyList multiReplyList={multiReplyList}/>
-      {
-        multiReply && <MultiReplyInput setMultiReply={setMultiReply}/>
-      }
+      )}
+      <StarMultiReplyList multiReplyList={multiReplyList} />
+      {multiReply && <MultiReplyInput setMultiReply={setMultiReply} />}
     </div>
   );
 }
 
-function MultiReplyInput(props){
+function MultiReplyInput(props) {
   const input = useRef(null);
-  const handleMultiReplyQuit = () => {props.setMultiReply(false)}
+  const handleMultiReplyQuit = () => {
+    props.setMultiReply(false);
+  };
   const handleMultiReplySubmit = async () => {
-
     const data = {
-      "commentIndex": 1, // 코멘트번호?
-      "boardIndex": 1, // 글 번호
-      "memberIndex": 1, // 작성자
-      "commentContent": "답댓글입니다" // 대댓글 내용
-    }
+      commentIndex: 1, // 코멘트번호?
+      boardIndex: 1, // 글 번호
+      memberIndex: 1, // 작성자
+      commentContent: "답댓글입니다", // 대댓글 내용
+    };
 
-    await axios.post(`${process.env.REACT_APP_API_URL}/multicomment/`,data,
-    {
-      headers: {
-        token: localStorage.getItem('token'),
-      },
-    })
-    .then((response) => {
-      if(response.data.map.response === 'success'){
-        props.setMultiReply(false);
-      }
-    })
-    .catch((error) => console.log(error))
-  }
+    await axios
+      .post(`${process.env.REACT_APP_API_URL}/multicomment/`, data, {
+        headers: {
+          token: localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        if (response.data.map.response === "success") {
+          props.setMultiReply(false);
+        }
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div>
-      └ <input ref={input}/>
-      <button onClick={() => {handleMultiReplyQuit()}}>취소</button>
-      <button onClick={() => {handleMultiReplySubmit()}}>등록</button>
+      └ <input ref={input} />
+      <button
+        onClick={() => {
+          handleMultiReplyQuit();
+        }}
+      >
+        취소
+      </button>
+      <button
+        onClick={() => {
+          handleMultiReplySubmit();
+        }}
+      >
+        등록
+      </button>
     </div>
-  )
+  );
 }
 
 export default StarReplyListItem;
