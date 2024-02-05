841f108e7f2810db322e3428d71a15e7924fde5f
조한빈 <whgksqls9999@naver.com>
2024-02-05 17:01:17 +0900

    Chore: Modal Space 추가
    Change-Id: Ie3349cc926407fc0579d996daa57d12502cd447d

--- FE/src/pages/MainPage.js
+++ FE/src/pages/MainPage.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 // import Login from '../components/login/Login'
 // import Regist from '../components/login/Regist'
-import Sidebar from '../components/Sidebar';
+import Sidebar from "../components/Sidebar";
 
-import ChangeInfo from '../components/user/ChangeInfo';
-import StarList from '../components/star/StarList';
-import ErrorPage from './ErrorPage';
-import UserSpace from 'components/user/UserSpace';
-import axios from 'axios';
+import ChangeInfo from "../components/user/ChangeInfo";
+import StarList from "../components/star/StarList";
+import ErrorPage from "./ErrorPage";
+import UserSpace from "components/user/UserSpace";
+import axios from "axios";
+import ModalSpace from "components/ModalSpace";
 // import StarFavoList from './star/StarFavorList'
 // import FollowList from './user/FollowList'
 // import FindUser from './user/FindUser'
@@ -19,12 +20,13 @@ import axios from 'axios';
 
 //로그인 했을때 메인페이지로 온다.
 export default function MainPage() {
-    return (
-        <div className="MainPage">
-            <div className="mainContainer" style={{ position: 'relative' }}>
-                <UserSpace className="asdf" />
-                <Outlet />
-            </div>
-        </div>
-    );
+  return (
+    <div className="MainPage">
+      <div className="mainContainer" style={{ position: "relative" }}>
+        <UserSpace className="asdf" />
+        <Outlet />
+        <ModalSpace />
+      </div>
+    </div>
+  );
 }
