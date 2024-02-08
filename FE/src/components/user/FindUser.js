b6dc2f2b4c4c23b42342704a42ea1b6ed4fdf97b
euny0ung <hdtv0218@gmail.com>
2024-02-08 10:38:55 +0900

    Style: 유저 검색 CSS 완료
    Change-Id: I57eabc733a5ba267e11dba0ba2120098f9338bb4

--- FE/src/components/user/FindUser.js
+++ FE/src/components/user/FindUser.js
@@ -5,6 +5,7 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { FaUserCircle } from 'react-icons/fa';
 import SearchBar from '../reusable/SearchBar';
 import { filterState, listState } from 'components/atom';
+import { TbHomeMove } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -39,26 +40,28 @@ function FindUser() {
     // 검색 결과와 일치하는 유저 닉네임 렌더링
     return (
         <div className="w-full h-full absolute top-0 left-0 flex justify-center items-center">
-            <Card className=" w-3/12 card-contain-style py-3">
-                <div className="searchArea flex justify-between items-center search-input mx-auto my-3">
-                    <div className="flex px-2">
+            <Card className=" w-1/5 card-contain-style py-3 ">
+                <div className="searchArea flex justify-between items-center search-input w-72 mx-auto my-3">
+                    <div className="px-2">
                         <SearchBar filterKey="memberNickname" />
                     </div>
                 </div>
-                <ScrollArea className=" h-96 overflow-auto border">
+                <ScrollArea className=" h-96 overflow-auto mx-7">
                     <div className="userList">
                         {filterData.map((it) => (
                             <li
                                 key={it.memberIndex}
-                                className="flex justify-start p-1"
+                                className="flex p-2 text-lg"
                             >
                                 <FaUserCircle
-                                    size="24"
+                                    size="30"
                                     className="pr-2 text-btn-bg-hover"
                                 />
                                 {it.memberNickname}
                                 <Link to={`/space/${it.memberIndex}`}>
-                                    <button>이동하기</button>
+                                    <TbHomeMove className="size-7 mx-2">
+                                        이동하기
+                                    </TbHomeMove>
                                 </Link>
                             </li>
                         ))}
