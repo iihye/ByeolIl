7e3a6b16e9dbffd020c261f81751bb83ddecff22
조한빈 <whgksqls9999@naver.com>
2024-02-09 14:11:33 +0900

    Feat: 등록된 댓글이 없습니다 추가
    Change-Id: I49785ca47c547907cc0b3c8d440cb5486b5c0a4f

--- FE/src/components/star/StarReplyList.js
+++ FE/src/components/star/StarReplyList.js
@@ -49,10 +49,14 @@ function StarReplyList(props) {
         <div>댓글 목록</div>
         <IoMdRefresh className="my-1 hover:cursor-pointer flex" onClick={handleRefresh} />
       </div>
-      <div className="star-reply-list min-h-20  max-h-72 border border-white-sub rounded-xl overflow-y-scroll bg-alert-bg text-white-sub" ref={replyListRef}>
-        {data.map((reply, index) => (
-          <StarReplyListItem reply={reply} key={index} boardIndex={boardIndex} />
-        ))}
+      <div className="star-reply-list relative min-h-20  max-h-72 border border-white-sub rounded-xl overflow-y-scroll bg-alert-bg text-white-sub" ref={replyListRef}>
+        {data[0] ? (
+          data.map((reply, index) => <StarReplyListItem reply={reply} key={index} boardIndex={boardIndex} />)
+        ) : (
+          <div className="flex absolute top-0 bottom-0 left-0 right-0 justify-center items-center h-full">
+            <div>등록된 댓글이 없습니다.</div>
+          </div>
+        )}
       </div>
     </>
   );
