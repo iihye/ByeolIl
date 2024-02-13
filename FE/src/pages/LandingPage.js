d59f524748b1cdf4c5e253eda20ea2088954b589
euny0ung <hdtv0218@gmail.com>
2024-02-13 10:25:16 +0900

    Fix: 화면 크기 꽉차게 조정
    Change-Id: Id22c8c5485d1e808e8134dd8e620e78e9bc2d774

--- FE/src/pages/LandingPage.js
+++ FE/src/pages/LandingPage.js
@@ -14,21 +14,24 @@ export default function LandingPage() {
     }, [location]);
 
     return (
-        <div>
-            <div className="relative flex justify-center items-center">
-                <div>
-                    <video autoPlay loop muted className="w-screen ">
-                        <source src={video} type="video/mp4"></source>
-                    </video>
-                </div>
-                <div className="content absolute">
-                    {!isOpen && (
-                        <h1 className="text-white text-6xl text-center font-['Star'] py-4">
-                            별일
-                        </h1>
-                    )}
-                    <Outlet />
-                </div>
+        <div className="relative flex justify-center items-center w-screen h-screen overflow-hidden">
+            <div>
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className="absolute top-0 left-0 w-full h-full object-cover"
+                >
+                    <source src={video} type="video/mp4"></source>
+                </video>
+            </div>
+            <div className="content absolute">
+                {!isOpen && (
+                    <h1 className="text-white text-6xl text-center font-['Star'] py-4">
+                        별일
+                    </h1>
+                )}
+                <Outlet />
             </div>
         </div>
     );
