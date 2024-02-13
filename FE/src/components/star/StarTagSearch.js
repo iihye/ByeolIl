85fb4c115d014cf3484106f4e02ac23fffd1df0e
이혜진 <floral_berry@naver.com>
2024-02-13 17:39:54 +0900

    Style: StarTagSearch css 수정
    Change-Id: Iffd844c172774d152e5bb0a653507f2db3725000

--- FE/src/components/star/StarTagSearch.js
+++ FE/src/components/star/StarTagSearch.js
@@ -1,19 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Card } from '@/components/ui/card';
-import { FaSearch } from 'react-icons/fa';
-import { IoCloseSharp } from 'react-icons/io5';
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
-import { useNavigate } from 'react-router';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { FaSearch } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { useNavigate } from "react-router";
+import { HiMiniHashtag } from "react-icons/hi2";
+import { FaRegFaceSadTear } from "react-icons/fa6";
 
 // 추후 카드 형식으로 나오게 css 변경
 // 추후 input 창 위에 tag가 올라가게 css 변경
 // 유효성 검사 다시 꼼꼼하게
 
 function StarTagSearch() {
-    const [tag, setTag] = useState('');
+    const [tag, setTag] = useState("");
     const [tagSearchData, setTagSearchData] = useState([]);
-    const [replaceTag, setReplaceTag] = useState('');
+    const [replaceTag, setReplaceTag] = useState("");
 
     const navigate = useNavigate();
 
@@ -22,9 +31,9 @@ function StarTagSearch() {
     };
 
     const handleList = () => {
-        setTag(' ');
+        setTag(" ");
         setTagSearchData([]);
-        setReplaceTag('');
+        setReplaceTag("");
     };
 
     const fetchData = async (tag) => {
@@ -41,31 +50,31 @@ function StarTagSearch() {
     };
 
     const activeSearch = (e) => {
-        if (e.key === 'Enter') {
+        if (e.key === "Enter") {
             e.preventDefault();
             activeButton();
-        } else if (e.key === 'Backspace') {
+        } else if (e.key === "Backspace") {
             handleList();
         }
     };
 
     const activeButton = () => {
-        if (tag === '') return;
+        if (tag === "") return;
 
         const specialRegExp = /[\{\}\[\]\/?.;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
 
         const testedTag = specialRegExp.test(tag)
-            ? tag.replace(specialRegExp, '')
+            ? tag.replace(specialRegExp, "")
             : tag;
 
-        setReplaceTag(testedTag.replace(/\s/g, ''));
+        setReplaceTag(testedTag.replace(/\s/g, ""));
 
         // 해시태그 모양 만들어주기
-        setTag(' ');
+        setTag(" ");
     };
 
     useEffect(() => {
-        if (replaceTag !== '') {
+        if (replaceTag !== "") {
             fetchData(replaceTag);
 
             const reverseData = [...tagSearchData].reverse();
@@ -77,27 +86,33 @@ function StarTagSearch() {
         function handleClick(e) {
             e.stopPropagation();
             const check = [...e.target.classList].some(
-                (it) => it === 'outside'
+                (it) => it === "outside"
             );
             if (check) {
                 navigate(-1);
             }
         }
 
-        window.addEventListener('click', handleClick);
+        window.addEventListener("click", handleClick);
         return () => {
-            window.removeEventListener('click', handleClick);
+            window.removeEventListener("click", handleClick);
         };
     });
 
     return (
         <div className="outside w-full h-full absolute top-0 left-0 flex justify-center items-center z-10 bg-modal-outside">
-            <Card className=" w-cardContainer card-contain-style py-3">
-                <div className="searchArea flex justify-between items-center search-input mx-auto my-3">
-                    <div className="flex px-2">
+            <Card className=" w-cardContainer card-contain-style px-6 py-6 ">
+                <CardHeader className="flex">
+                    <CardTitle className="flex justify-start items-center font-['Pre-Bold'] text-2xl">
+                        <HiMiniHashtag className="mr-1" />
+                        태그로 별 찾기
+                    </CardTitle>
+                </CardHeader>
+                <div className="searchArea flex justify-between items-center search-input mx-auto my-3 ">
+                    <div className="flex px-2 font-['Pre-Bold']">
                         {replaceTag && (
                             <span
-                                className="text-black-sub flex items-center p-2"
+                                className="text-black-sub flex items-center p-2 w-1/2"
                                 onClick={handleList}
                             >
                                 #{replaceTag}
@@ -109,7 +124,7 @@ function StarTagSearch() {
                             value={tag}
                             onChange={handleTag}
                             onKeyDown={(e) => activeSearch(e)}
-                            className=" text-black-sub my-3"
+                            className=" text-black-sub my-3 w-1/2"
                         />
                     </div>
 
@@ -128,7 +143,7 @@ function StarTagSearch() {
                                         key={it.boardIndex}
                                         className="card-style h-80 w-64"
                                     >
-                                        <div className="cards w-4/5 mx-auto ">
+                                        <div className="cards w-4/5 mx-auto font-['Pre-Light']">
                                             <div className="cardInfo text-xs py-2 pl-1.5">
                                                 작성일&nbsp;
                                                 <strong>
@@ -151,7 +166,12 @@ function StarTagSearch() {
                                     </Card>
                                 ))
                             ) : (
-                                <div>검색 결과가 없습니다</div>
+                                <div className="Card-ScrollArea-NonResult h-96 flex flex-col col-span-3 justify-center items-center">
+                                    <FaRegFaceSadTear className="mr-1" />
+                                    <div className="font-['Pre-Bold']">
+                                        일치하는 결과가 없습니다
+                                    </div>
+                                </div>
                             )
                         ) : null}
                     </div>
