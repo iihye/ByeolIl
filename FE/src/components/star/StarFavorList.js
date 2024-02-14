75ef05761c760fe21bdc5e16b6b458f8354ecece
euny0ung <hdtv0218@gmail.com>
2024-02-14 13:59:29 +0900

    Fix: 비밀글 필터처리
    Change-Id: Ic4f5e24e2cb865c2636c37dd9841e6dc1cb659c8

--- FE/src/components/star/StarFavorList.js
+++ FE/src/components/star/StarFavorList.js
@@ -1,35 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { filterState, isFavorListOpenState, isStarDetailOpenState } from "components/atom";
-import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
-import SearchBar from "../reusable/SearchBar";
-import { FaHeart } from "react-icons/fa";
-import axios from "axios";
-import { useNavigate } from "react-router";
-import { FaSearch } from "react-icons/fa";
-import { LuFolderHeart } from "react-icons/lu";
-import { FaRegFaceSadTear } from "react-icons/fa6";
+import React, { useEffect, useState } from 'react';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import {
+    filterState,
+    isFavorListOpenState,
+    isStarDetailOpenState,
+} from 'components/atom';
+import { useRecoilState, useRecoilValue, useResetRecoilState } from 'recoil';
+import SearchBar from '../reusable/SearchBar';
+import { FaHeart } from 'react-icons/fa';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+import { FaSearch } from 'react-icons/fa';
+import { LuFolderHeart } from 'react-icons/lu';
+import { FaRegFaceSadTear } from 'react-icons/fa6';
 
 function StarFavorList() {
-    const token = sessionStorage.getItem("token");
+    const token = sessionStorage.getItem('token');
     const resetIsFavorListOpen = useResetRecoilState(isFavorListOpenState);
-    const [listData, setListData] = useState("");
-    const [memberIndex, setMemberIndex] = useState(sessionStorage.getItem("memberIndex"));
-    const [starDetailState, setStarDetailState] = useRecoilState(isStarDetailOpenState);
+    const [listData, setListData] = useState('');
+    const [memberIndex, setMemberIndex] = useState(
+        sessionStorage.getItem('memberIndex')
+    );
+    const [starDetailState, setStarDetailState] = useRecoilState(
+        isStarDetailOpenState
+    );
     const resetList = useResetRecoilState(filterState);
     const filterData = useRecoilValue(filterState);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`${process.env.REACT_APP_API_URL}/board/like/${memberIndex}`, {
-                    headers: {
-                        token: token,
-                    },
-                });
+                const response = await axios.get(
+                    `${process.env.REACT_APP_API_URL}/board/like/${memberIndex}`,
+                    {
+                        headers: {
+                            token: token,
+                        },
+                    }
+                );
                 resetList();
-                setListData(response.data.filter((item) => item.boardAccess === "OPEN"));
+                setListData(
+                    response.data.filter((item) => item.boardAccess === 'OPEN')
+                );
             } catch (error) {
                 console.log(error);
             }
@@ -42,15 +62,17 @@ function StarFavorList() {
         function handleClick(e) {
             e.stopPropagation();
 
-            const check = [...e.target.classList].some((it) => it === "outside");
+            const check = [...e.target.classList].some(
+                (it) => it === 'outside'
+            );
             if (check) {
                 resetIsFavorListOpen();
             }
         }
 
-        window.addEventListener("click", handleClick);
+        window.addEventListener('click', handleClick);
         return () => {
-            window.removeEventListener("click", handleClick);
+            window.removeEventListener('click', handleClick);
         };
     });
 
@@ -68,7 +90,10 @@ function StarFavorList() {
                 <CardContent>
                     <div className="searchArea flex justify-between items-center search-input mx-auto my-6">
                         <div className="flex px-2">
-                            <SearchBar filterKey="boardContent" listItems={listData} />
+                            <SearchBar
+                                filterKey="boardContent"
+                                listItems={listData}
+                            />
                         </div>
 
                         <FaSearch size="20" className="text-black-sub mx-3" />
@@ -81,31 +106,50 @@ function StarFavorList() {
                                         <Card
                                             key={it.boardIndex}
                                             className="card-style h-80 w-64 relative"
-                                            onClick={() => setStarDetailState([it.boardIndex, it.memberIndex])}
+                                            onClick={() =>
+                                                setStarDetailState([
+                                                    it.boardIndex,
+                                                    it.memberIndex,
+                                                ])
+                                            }
                                         >
                                             <div className="cards w-4/5 mx-auto font-['Pre-Light']">
                                                 <div className="cardInfo text-xs py-2 pl-1.5">
                                                     작성일&nbsp;
-                                                    <strong>{it.boardInputDate}</strong>
+                                                    <strong>
+                                                        {it.boardInputDate}
+                                                    </strong>
                                                     &nbsp; | 작성자&nbsp;
-                                                    <strong>{it.memberNickname}</strong>
+                                                    <strong>
+                                                        {it.memberNickname}
+                                                    </strong>
                                                 </div>
-                                                <div className="cardContent py-2">{it.boardContent}</div>
+                                                <div className="cardContent py-2">
+                                                    {it.boardContent}
+                                                </div>
                                                 <div className="absolute bottom-0 w-10/12 mb-3">
                                                     <div className="cardTag flex py-2 ">
                                                         {it.hash
                                                             ? it.hash.length > 0
-                                                                ? it.hash.map((tag) => (
-                                                                      <div>
-                                                                          #{tag}
-                                                                          &nbsp;
-                                                                      </div>
-                                                                  ))
+                                                                ? it.hash.map(
+                                                                      (tag) => (
+                                                                          <div>
+                                                                              #
+                                                                              {
+                                                                                  tag
+                                                                              }
+                                                                              &nbsp;
+                                                                          </div>
+                                                                      )
+                                                                  )
                                                                 : null
                                                             : null}
                                                     </div>
                                                     <div className="cardLike flex justify-end">
-                                                        <FaHeart size="24" className="mr-1.5" />
+                                                        <FaHeart
+                                                            size="24"
+                                                            className="mr-1.5"
+                                                        />
                                                         {it.boardHeart}
                                                     </div>
                                                 </div>
@@ -115,7 +159,9 @@ function StarFavorList() {
                                 ) : (
                                     <div className="Card-ScrollArea-NonResult h-96 flex flex-col col-span-3 justify-center items-center">
                                         <FaRegFaceSadTear className="mr-1" />
-                                        <div className="font-['Pre-Bold']">일치하는 결과가 없습니다</div>
+                                        <div className="font-['Pre-Bold']">
+                                            일치하는 결과가 없습니다
+                                        </div>
                                     </div>
                                 )
                             ) : null}
--- FE/src/components/star/StarTagSearch.js
+++ FE/src/components/star/StarTagSearch.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { FaSearch } from "react-icons/fa";
-import { IoCloseSharp } from "react-icons/io5";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { useNavigate } from "react-router";
-import { HiMiniHashtag } from "react-icons/hi2";
-import { FaRegFaceSadTear } from "react-icons/fa6";
-import { useResetRecoilState } from "recoil";
-import { isTagSearchOpenState } from "components/atom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { FaSearch } from 'react-icons/fa';
+import { IoCloseSharp } from 'react-icons/io5';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { useNavigate } from 'react-router';
+import { HiMiniHashtag } from 'react-icons/hi2';
+import { FaRegFaceSadTear } from 'react-icons/fa6';
+import { useResetRecoilState } from 'recoil';
+import { isTagSearchOpenState } from 'components/atom';
 
 // 추후 카드 형식으로 나오게 css 변경
 // 추후 input 창 위에 tag가 올라가게 css 변경
@@ -17,53 +24,61 @@ import { isTagSearchOpenState } from "components/atom";
 function StarTagSearch() {
     const resetIsTagSearchOpen = useResetRecoilState(isTagSearchOpenState);
 
-    const [tag, setTag] = useState("");
+    const [tag, setTag] = useState('');
     const [tagSearchData, setTagSearchData] = useState([]);
-    const [replaceTag, setReplaceTag] = useState("");
+    const [replaceTag, setReplaceTag] = useState('');
 
     const handleTag = (e) => {
         setTag(e.target.value);
     };
 
     const handleList = () => {
-        setTag(" ");
+        setTag(' ');
         setTagSearchData([]);
-        setReplaceTag("");
+        setReplaceTag('');
     };
 
     const fetchData = async (tag) => {
         await axios
-            .get(`${process.env.REACT_APP_API_URL}/search?tag=${encodeURIComponent(tag)}`)
+            .get(
+                `${
+                    process.env.REACT_APP_API_URL
+                }/search?tag=${encodeURIComponent(tag)}`
+            )
             .then((response) => {
-                setTagSearchData(response.data);
+                setTagSearchData(
+                    response.data.filter((item) => item.boardAccess === 'OPEN')
+                );
             })
             .catch((e) => console.log(e));
     };
 
     const activeSearch = (e) => {
-        if (e.key === "Enter") {
+        if (e.key === 'Enter') {
             e.preventDefault();
             activeButton();
-        } else if (e.key === "Backspace") {
+        } else if (e.key === 'Backspace') {
             handleList();
         }
     };
 
     const activeButton = () => {
-        if (tag === "") return;
+        if (tag === '') return;
 
         const specialRegExp = /[\{\}\[\]\/?.;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
 
-        const testedTag = specialRegExp.test(tag) ? tag.replace(specialRegExp, "") : tag;
+        const testedTag = specialRegExp.test(tag)
+            ? tag.replace(specialRegExp, '')
+            : tag;
 
-        setReplaceTag(testedTag.replace(/\s/g, ""));
+        setReplaceTag(testedTag.replace(/\s/g, ''));
 
         // 해시태그 모양 만들어주기
-        setTag(" ");
+        setTag(' ');
     };
 
     useEffect(() => {
-        if (replaceTag !== "") {
+        if (replaceTag !== '') {
             fetchData(replaceTag);
 
             const reverseData = [...tagSearchData].reverse();
@@ -74,15 +89,17 @@ function StarTagSearch() {
     useEffect(() => {
         function handleClick(e) {
             e.stopPropagation();
-            const check = [...e.target.classList].some((it) => it === "outside");
+            const check = [...e.target.classList].some(
+                (it) => it === 'outside'
+            );
             if (check) {
                 resetIsTagSearchOpen();
             }
         }
 
-        window.addEventListener("click", handleClick);
+        window.addEventListener('click', handleClick);
         return () => {
-            window.removeEventListener("click", handleClick);
+            window.removeEventListener('click', handleClick);
         };
     });
 
@@ -98,7 +115,10 @@ function StarTagSearch() {
                 <div className="searchArea flex justify-between items-center search-input mx-auto my-3 ">
                     <div className="flex px-2 font-['Pre-Bold']">
                         {replaceTag && (
-                            <span className="text-black-sub flex items-center p-2 w-1/2" onClick={handleList}>
+                            <span
+                                className="text-black-sub flex items-center p-2 w-1/2"
+                                onClick={handleList}
+                            >
                                 #{replaceTag}
                                 <IoCloseSharp />
                             </span>
@@ -112,37 +132,46 @@ function StarTagSearch() {
                         />
                     </div>
 
-                    <FaSearch size="20" className="text-black-sub mx-3" onClick={() => activeButton()} />
+                    <FaSearch
+                        size="20"
+                        className="text-black-sub mx-3"
+                        onClick={() => activeButton()}
+                    />
                 </div>
                 <ScrollArea className=" h-96 overflow-auto">
                     <div className="grid grid-cols-3 justify-items-center gap-4">
-                        {tagSearchData ? (
-                            tagSearchData.length > 0 ? (
-                                tagSearchData.map((it) => (
-                                    <Card key={it.boardIndex} className="card-style h-80 w-64">
-                                        <div className="cards w-4/5 mx-auto font-['Pre-Light']">
-                                            <div className="cardInfo text-xs py-2 pl-1.5">
-                                                작성일&nbsp;
-                                                <strong>{it.boardInputDate}</strong>
-                                                &nbsp; | 작성자&nbsp;
-                                                <strong>{it.memberNickname}</strong>
-                                            </div>
-                                            <div className="cardContent py-2">{it.boardContent}</div>
-                                            <div className="cardTag flex py-2">
-                                                {it.hash.map((tag) => (
-                                                    <div>#{tag}&nbsp;</div>
-                                                ))}
-                                            </div>
+                        {tagSearchData?.length > 0 ? (
+                            tagSearchData.map((it) => (
+                                <Card
+                                    key={it.boardIndex}
+                                    className="card-style h-80 w-64"
+                                >
+                                    <div className="cards w-4/5 mx-auto font-['Pre-Light']">
+                                        <div className="cardInfo text-xs py-2 pl-1.5">
+                                            작성일&nbsp;
+                                            <strong>{it.boardInputDate}</strong>
+                                            &nbsp; | 작성자&nbsp;
+                                            <strong>{it.memberNickname}</strong>
                                         </div>
-                                    </Card>
-                                ))
-                            ) : (
-                                <div className="Card-ScrollArea-NonResult h-96 flex flex-col col-span-3 justify-center items-center">
-                                    <FaRegFaceSadTear className="mr-1" />
-                                    <div className="font-['Pre-Bold']">일치하는 결과가 없습니다</div>
+                                        <div className="cardContent py-2">
+                                            {it.boardContent}
+                                        </div>
+                                        <div className="cardTag flex py-2">
+                                            {it.hash.map((tag) => (
+                                                <div>#{tag}&nbsp;</div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </Card>
+                            ))
+                        ) : (
+                            <div className="Card-ScrollArea-NonResult h-96 flex flex-col col-span-3 justify-center items-center">
+                                <FaRegFaceSadTear className="mr-1" />
+                                <div className="font-['Pre-Bold']">
+                                    일치하는 결과가 없습니다
                                 </div>
-                            )
-                        ) : null}
+                            </div>
+                        )}
                     </div>
                 </ScrollArea>
             </Card>
