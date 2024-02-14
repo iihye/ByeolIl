2e03f07e46bcdd838c0a791f0391e578ef43621c
이혜진 <floral_berry@naver.com>
2024-02-14 12:14:32 +0900

    Style: Sidebar icon 추가
    Change-Id: I76b2262a7a473fcaf2671595139308f0cfbca211

--- FE/src/components/Sidebar.js
+++ FE/src/components/Sidebar.js
@@ -3,10 +3,18 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useVideoTexture } from "@react-three/drei";
 import { IoMdMenu } from "react-icons/io";
+import * as RiIcons from "react-icons/ri";
+import * as WiIcons from "react-icons/wi";
+import * as LuIcons from "react-icons/lu";
+import * as AiIcons from "react-icons/ai";
+import * as PiIcons from "react-icons/pi";
+import * as HiIcons from "react-icons/hi2";
+import * as IoIcons from "react-icons/io5";
 
 function SidebarList(props) {
     const [items, setItems] = useState([]);
     const isAdmin = sessionStorage.getItem("auth");
+    const nickname = sessionStorage.getItem("nickname");
 
     const navigate = useNavigate();
 
@@ -21,30 +29,47 @@ function SidebarList(props) {
     useEffect(() => {
         setItems([
             {
+                type: "RiIcons",
+                icon: "RiLockPasswordLine",
                 name: "회원정보수정",
                 path: `/space/${props.memberIndex}/editInfo`,
             },
             {
+                type: "WiIcons",
+                icon: "WiStars",
                 name: "나의 별 목록",
                 path: `/space/${props.memberIndex}/starMine`,
             },
             {
+                type: "LuIcons",
+                icon: "LuFolderHeart",
                 name: "좋아하는 별 목록",
                 path: `/space/${props.memberIndex}/starFavor`,
             },
             {
+                type: "AiIcons",
+                icon: "AiOutlineUserAdd",
                 name: "팔로우/팔로워 목록",
                 path: `/space/${props.memberIndex}/follow`,
             },
             {
+                type: "PiIcons",
+                icon: "PiShootingStarLight",
                 name: "다른 우주 찾기",
                 path: `/space/${props.memberIndex}/findUser`,
             },
             {
+                type: "HiIcons",
+                icon: "HiMiniHashtag",
                 name: "태그로 별 찾기",
                 path: `/space/${props.memberIndex}/tagSearch`,
             },
-            { name: "환경설정", path: `/space/${props.memberIndex}/settings` },
+            {
+                type: "IoIcons",
+                icon: "IoSettingsOutline",
+                name: "환경설정",
+                path: `/space/${props.memberIndex}/settings`,
+            },
         ]);
     }, []);
 
@@ -52,20 +77,51 @@ function SidebarList(props) {
         if (isAdmin == "ROLE_ADMIN")
             setItems((prevItems) => [
                 ...prevItems,
-                { name: "신고관리", path: `/space/admin/report` },
+                {
+                    type: "PiIcons",
+                    icon: "PiSiren",
+                    name: "신고관리",
+                    path: `/space/admin/report`,
+                },
             ]);
     }, [props.memberIndex]);
 
     return (
         <div className="sidebarList bg-modal-bg text-white-sub p-2 rounded-xl">
-            <h2>{props.name}님의 우주</h2>
+            <h2 className="mb-2">{nickname}님의 우주</h2>
             {/* 땡땡님의 우주 옆에 연필 아이콘(닉네임 수정 모달창으로 이동) */}
             {items.map((item, index) => {
+                let IconComponent;
+
+                if (item.type === "RiIcons") {
+                    IconComponent = RiIcons[item.icon];
+                } else if (item.type === "WiIcons") {
+                    IconComponent = WiIcons[item.icon];
+                } else if (item.type === "LuIcons") {
+                    IconComponent = LuIcons[item.icon];
+                } else if (item.type === "AiIcons") {
+                    IconComponent = AiIcons[item.icon];
+                } else if (item.type === "PiIcons") {
+                    IconComponent = PiIcons[item.icon];
+                } else if (item.type === "HiIcons") {
+                    IconComponent = HiIcons[item.icon];
+                } else if (item.type === "IoIcons") {
+                    IconComponent = IoIcons[item.icon];
+                }
+
                 return (
-                    <div className="sidebarItem" key={index}>
-                        <Link to={item.path}>
-                            <p className="mb-3">{item.name}</p>
-                        </Link>
+                    <div
+                        className="flex justyfy-center sidebarItem mb-2"
+                        key={index}
+                    >
+                        <div className="flex justyfy-center items-center">
+                            {IconComponent && (
+                                <IconComponent className="mr-2" />
+                            )}
+                            <Link to={item.path}>
+                                <p>{item.name}</p>
+                            </Link>
+                        </div>
                     </div>
                 );
             })}
