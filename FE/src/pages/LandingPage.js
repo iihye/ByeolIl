d27303f80b1d0589980b93ac62fb069627a24bae
euny0ung <hdtv0218@gmail.com>
2024-02-15 02:25:09 +0900

    Feat: 로딩 화면
    Change-Id: I15f0c7191dd9e38c237f59464d66a90a1ffc120e

--- FE/src/pages/LandingPage.js
+++ FE/src/pages/LandingPage.js
@@ -1,9 +1,9 @@
 // import video from "../video/background1.mp4"
 // import video from "../video/background2.mp4"
-import video from "../video/background3.mp4";
-import { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import { useResetRecoilState } from "recoil";
+import video from '../video/background3.mp4';
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useResetRecoilState } from 'recoil';
 import {
     isChangeInfoOpenState,
     isFavorListOpenState,
@@ -12,7 +12,7 @@ import {
     isMyStarListOpenState,
     isSettingOpenState,
     isTagSearchOpenState,
-} from "components/atom";
+} from 'components/atom';
 
 export default function LandingPage() {
     const resetIsChangeInfoOpen = useResetRecoilState(isChangeInfoOpenState);
@@ -27,8 +27,8 @@ export default function LandingPage() {
     const location = useLocation();
 
     useEffect(() => {
-        console.log(location.pathname.split("/").length === 3);
-        if (location.pathname.split("/").length > 2) setIsOpen(true);
+        console.log(location.pathname.split('/').length === 3);
+        if (location.pathname.split('/').length > 2) setIsOpen(true);
         else setIsOpen(false);
     }, [location]);
 
@@ -46,12 +46,21 @@ export default function LandingPage() {
     return (
         <div className="relative flex justify-center items-center w-screen h-screen overflow-hidden">
             <div>
-                <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover">
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className="absolute top-0 left-0 w-full h-full object-cover"
+                >
                     <source src={video} type="video/mp4"></source>
                 </video>
             </div>
             <div className="content absolute">
-                {!isOpen && <h1 className="text-white text-6xl text-center font-['Star'] py-4">별일</h1>}
+                {!isOpen && (
+                    <h1 className="text-white text-6xl text-center font-['Star'] py-4">
+                        별일
+                    </h1>
+                )}
                 <Outlet />
             </div>
         </div>
