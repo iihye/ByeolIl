34bf3f804dc6752b654fec62136f52e74b66f1cd
euny0ung <hdtv0218@gmail.com>
2024-02-14 14:59:19 +0900

    Fix: 환경설정 초기값 수정중
    Change-Id: I48650db78a4f4bba71bb357696598e17936539cb

--- FE/src/components/user/Settings.js
+++ FE/src/components/user/Settings.js
@@ -1,38 +1,46 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { IoSettingsOutline } from "react-icons/io5";
-import { PiRadioFill } from "react-icons/pi";
-import { Slider } from "@/components/ui/slider";
-import { useResetRecoilState } from "recoil";
-import { isSettingOpenState } from "components/atom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { PiRadioFill } from 'react-icons/pi';
+import { Slider } from '@/components/ui/slider';
+import { useResetRecoilState } from 'recoil';
+import { isSettingOpenState } from 'components/atom';
 
 function Settings() {
     const resetIsSettingOpen = useResetRecoilState(isSettingOpenState);
 
-    const options = [
-        { name: "흐린", value: "OLD" },
-        { name: "아련한", value: "OLDER" },
-        { name: "어렴풋한", value: "OLDEST" },
-    ];
+    const options = ['OLD', 'OLDER', 'OLDEST'];
 
-    const [selectedOption, setSelectedOption] = useState("");
-    const memberIndex = sessionStorage.getItem("memberIndex");
-    const userToken = sessionStorage.getItem("token");
+    const [selectedOption, setSelectedOption] = useState('');
+    const [initOption, setInitOption] = useState('');
+    const memberIndex = sessionStorage.getItem('memberIndex');
+    const userToken = sessionStorage.getItem('token');
 
     useEffect(() => {
         // 유저 정보 가져오기
         const getUserIndex = async () => {
             try {
-                const response = await axios.get(`${process.env.REACT_APP_API_URL}/member/info/mine`, {
-                    headers: {
-                        token: sessionStorage.getItem("token"),
-                    },
-                });
+                const response = await axios.get(
+                    `${process.env.REACT_APP_API_URL}/member/info/mine`,
+                    {
+                        headers: {
+                            token: sessionStorage.getItem('token'),
+                        },
+                    }
+                );
 
                 setSelectedOption(response.data.memberRadioStatus);
+
+                // option에서 selectedOption과 일치하는 배열 index 가져오기
+                if (selectedOption) {
+                    const result = options.findIndex(
+                        (it) => it === selectedOption
+                    );
+                    setInitOption(result);
+                }
             } catch (error) {
-                console.log("회원정보 가져오기 실패", error);
+                console.log('회원정보 가져오기 실패', error);
             }
         };
 
@@ -42,7 +50,7 @@ function Settings() {
     // 슬라이더 값에 따른 처리 함수
     const handleSliderChange = (valueArray) => {
         const sliderValue = valueArray[0]; // 슬라이더는 하나의 값만 반환
-        setSelectedOption(options[sliderValue].value); // 슬라이더 값에 해당하는 옵션 선택
+        setSelectedOption(options[sliderValue]); // 슬라이더 값에 해당하는 옵션 선택
 
         if (selectedOption) {
             axios
@@ -59,7 +67,7 @@ function Settings() {
                     }
                 )
                 .then(() => {
-                    alert("변경되었습니다.");
+                    alert('변경되었습니다.');
                 })
                 .catch((error) => {
                     console.log(error);
@@ -71,15 +79,17 @@ function Settings() {
         function handleClick(e) {
             e.stopPropagation();
 
-            const check = [...e.target.classList].some((it) => it === "outside");
+            const check = [...e.target.classList].some(
+                (it) => it === 'outside'
+            );
             if (check) {
                 resetIsSettingOpen();
             }
         }
 
-        window.addEventListener("click", handleClick);
+        window.addEventListener('click', handleClick);
         return () => {
-            window.removeEventListener("click", handleClick);
+            window.removeEventListener('click', handleClick);
         };
     }, []);
 
@@ -93,23 +103,29 @@ function Settings() {
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
-                    {selectedOption ? (
+                    {initOption ? (
                         <>
-                            {" "}
                             <div className="flex font-['Pre-bold'] mb-2">
-                                <PiRadioFill size="24" className="pr-2 text-btn-bg-hover" />
+                                <PiRadioFill
+                                    size="24"
+                                    className="pr-2 text-btn-bg-hover"
+                                />
                                 라디오 수신 범위 설정
                             </div>
                             <div className="flex justify-between pl-2 pr-2 mb-2">
-                                <div className="w-1/6 mr-2 font-['Pre-bold']">최근</div>
+                                <div className="w-1/6 mr-2 font-['Pre-bold']">
+                                    최근
+                                </div>
                                 <Slider
                                     className="Settings-Slider flex-grow"
-                                    defaultValue={[0]} // 기본값을 options 배열의 첫 번째 요소에 맞춤
+                                    defaultValue={[initOption]} // 기본값
                                     max={options.length - 1} // 최대값을 options 배열의 마지막 요소에 맞춤
                                     step={1}
                                     onValueChange={handleSliderChange} // 슬라이더 값이 변경될 때 호출될 함수
                                 />
-                                <div className="w-1/6 ml-2 font-['Pre-bold']">과거</div>
+                                <div className="w-1/6 ml-2 font-['Pre-bold']">
+                                    과거
+                                </div>
                             </div>
                         </>
                     ) : (
