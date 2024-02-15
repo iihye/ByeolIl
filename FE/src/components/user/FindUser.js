eedb374e30645b343fcab9cbc10c818098d22c84
euny0ung <hdtv0218@gmail.com>
2024-02-15 09:51:59 +0900

    Fix: 모달창 텍스트 변경
    Change-Id: I925e92e9efb3864c7805135b1ccb80e195db7be9

--- FE/src/components/user/FindUser.js
+++ FE/src/components/user/FindUser.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { useRecoilValue, useSetRecoilState, useResetRecoilState } from "recoil";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { FaUserCircle } from "react-icons/fa";
-import SearchBar from "../reusable/SearchBar";
-import { filterState, isFindUserOpenState } from "components/atom";
-import { PiShootingStarLight } from "react-icons/pi";
-import { TbHomeMove } from "react-icons/tb";
-import { Link, useNavigate } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { useRecoilValue, useSetRecoilState, useResetRecoilState } from 'recoil';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { FaUserCircle } from 'react-icons/fa';
+import SearchBar from '../reusable/SearchBar';
+import { filterState, isFindUserOpenState } from 'components/atom';
+import { PiShootingStarLight } from 'react-icons/pi';
+import { TbHomeMove } from 'react-icons/tb';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
+import axios from 'axios';
 
 // 유저 검색 기능
 function FindUser() {
     const resetIsFindUserOpen = useResetRecoilState(isFindUserOpenState);
-    const [listData, setListData] = useState("");
+    const [listData, setListData] = useState('');
     const filterData = useRecoilValue(filterState);
-    const userToken = sessionStorage.getItem("token") ?? "";
+    const userToken = sessionStorage.getItem('token') ?? '';
     const resetList = useResetRecoilState(filterState);
 
     const isEmpty = false;
 
     function handleMove(memberNickname) {
-        alert(`${memberNickname}의 우주로 이동합니다🚀`);
+        alert(`${memberNickname} 우주로 이동합니다🚀`);
         resetIsFindUserOpen(false);
     }
 
@@ -52,16 +52,16 @@ function FindUser() {
             e.stopPropagation();
 
             const check = [...e.target.classList].some(
-                (it) => it === "outside"
+                (it) => it === 'outside'
             );
             if (check) {
                 resetIsFindUserOpen();
             }
         }
 
-        window.addEventListener("click", handleClick);
+        window.addEventListener('click', handleClick);
         return () => {
-            window.removeEventListener("click", handleClick);
+            window.removeEventListener('click', handleClick);
         };
     });
 
