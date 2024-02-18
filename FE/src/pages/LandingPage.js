a26edb07eb29cbd94038413fe05d2416aa8dd9fb
kdh19583 <kdh19583@gmail.com>
2024-02-18 13:49:26 +0900

    Feat: 랜딩페이지 하단에 설명서 만드는중
    Change-Id: I164d0599da5c9924655e932cecfcb2d52649f20d

--- FE/src/pages/LandingPage.js
+++ FE/src/pages/LandingPage.js
@@ -1,7 +1,5 @@
-// import video from "../video/background1.mp4"
-// import video from "../video/background2.mp4"
 import video from '../video/background3.mp4';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Outlet, useLocation, Link } from 'react-router-dom';
 import { useResetRecoilState } from 'recoil';
 import {
@@ -26,6 +24,9 @@ export default function LandingPage() {
 
     const [isOpen, setIsOpen] = useState(false);
     const location = useLocation();
+    //스크롤페이지 상태관리
+    const [isInViewport, setIsInViewport] = useState(false);
+    const ref = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         if (location.pathname.split('/').length > 2) setIsOpen(true);
@@ -43,33 +44,62 @@ export default function LandingPage() {
         resetIsSettingOpen();
     }, []);
 
+    useEffect(() => {
+        if (!ref.current) return; // 요소가 아직 준비되지 않은 경우 중단
+      
+        const callback = (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              // 요소가 뷰포트에 나타났을 경우
+              setIsInViewport(true);
+            } else {
+              // 요소가 뷰포트를 벗어난 경우
+              setIsInViewport(false);
+            }
+          });
+        };
+      
+        const options = { root: null, rootMargin: "0px", threshold: 0 };
+        const observer = new IntersectionObserver(callback, options);
+        observer.observe(ref.current); // 요소 관찰 시작
+      
+        return () => {
+          observer.disconnect(); // 컴포넌트 언마운트 시 관찰 중단
+        };
+      }, []);
+      
     return (
         <div className="relative flex justify-center items-center w-screen h-screen overflow-hidden bg-black">
-            <div>
-                <video
-                    autoPlay
-                    loop
-                    muted
-                    className="absolute top-0 left-0 w-full h-full object-cover"
-                >
-                    <source src={video} type="video/mp4"></source>
-                </video>
-            </div>
-            <div className="content absolute">
-                {!isOpen && (
-                    <div className="font-['Pre-Bold'] w-56">
-                        <h1 className="text-white text-6xl text-center font-['Star'] py-4">
-                            별일
-                        </h1>
-                        <Link to="/landing/login">
-                            <Button className="w-full h-button my-1">
-                                시작하기
-                            </Button>
-                        </Link>
-                    </div>
-                )}
-                <Outlet />
-            </div>
+            <Container>
+                <div>
+                    <video
+                        autoPlay
+                        loop
+                        muted
+                        className="absolute top-0 left-0 w-full h-full object-cover"
+                    >
+                        <source src={video} type="video/mp4"></source>
+                    </video>
+                </div>
+                <div className="content absolute">
+                    {!isOpen && (
+                        <div className="font-['Pre-Bold'] w-56">
+                            <h1 className="text-white text-6xl text-center font-['Star'] py-4">
+                                별일
+                            </h1>
+                            <Link to="/landing/login">
+                                <Button className="w-full h-button my-1">
+                                    시작하기
+                                </Button>
+                            </Link>
+                        </div>
+                    )}
+                    <Outlet />
+                </div>
+            /</Container>
+            <Container className={isInViewport ? "frame-in" : ""} ref={ref}>
+                <h1>안녕하세요</h1>
+            </Container>
         </div>
     );
 }
