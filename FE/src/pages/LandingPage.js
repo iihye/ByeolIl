1e454fdd30cc324e8dea54d04a04ea710c0553a4
kkdongha <kdh19583@gmail.com>
2024-02-21 01:59:11 +0900

    Feat: 스크롤 설명서 구현
    Change-Id: Id3a86c8de97b2549dac138d87e97633a056fc6b7

--- FE/src/pages/LandingPage.js
+++ FE/src/pages/LandingPage.js
@@ -3,7 +3,7 @@ import create from "../img/create.gif"
 import hover from "../img/hover.gif"
 import move from "../img/move.gif"
 import scrollDown from "../img/scrollDownImg.png"
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, useLocation, Link } from 'react-router-dom';
 import { useResetRecoilState } from 'recoil';
 import {
@@ -30,9 +30,6 @@ export default function LandingPage() {
 
     const [isOpen, setIsOpen] = useState(false);
     const location = useLocation();
-    //스크롤페이지 상태관리
-    const [isInViewport, setIsInViewport] = useState(false);
-    const ref = useRef(null);
 
     useEffect(() => {
         if (location.pathname.split("/").length > 2) setIsOpen(true);
@@ -49,30 +46,6 @@ export default function LandingPage() {
         resetIsTagSearchOpen();
         resetIsSettingOpen();
     }, []);
-
-    useEffect(() => {
-        if (!ref.current) return; // 요소가 아직 준비되지 않은 경우 중단
-      
-        const callback = (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              // 요소가 뷰포트에 나타났을 경우
-              setIsInViewport(true);
-            } else {
-              // 요소가 뷰포트를 벗어난 경우
-              setIsInViewport(false);
-            }
-          });
-        };
-      
-        const options = { root: null, rootMargin: "0px", threshold: 0 };
-        const observer = new IntersectionObserver(callback, options);
-        observer.observe(ref.current); // 요소 관찰 시작
-      
-        return () => {
-          observer.disconnect(); // 컴포넌트 언마운트 시 관찰 중단
-        };
-      }, []);
       
     return (<div className="bg-slate-900 text-white">
         <Container>
@@ -101,9 +74,10 @@ export default function LandingPage() {
                         </div>
                     )}
                     <Outlet />
+                    {!isOpen &&
                     <div className='fixed bottom-0 m-2.5 ml-[60px]'>
                         <DownArrow src={scrollDown} />
-                    </div>
+                    </div>}
                 </div>
             </div>
             </Container>
